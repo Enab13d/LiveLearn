7a8632cb73ffef7c35839f78b61ffd44ea4b3a07
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-05-29 15:16:58 +0300

    US-F01 Solution scaffold and shared building
    - defined command and query handler contracts

---
+++ src/Shared/LiveLearn.BuildingBlocks/IQueryHandler.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace LiveLearn.BuildingBlocks;
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+    where TQuery : IQuery<TResponse>;
