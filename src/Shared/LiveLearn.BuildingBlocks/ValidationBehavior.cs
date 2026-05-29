ea88a4a63a052ccce7694e6534473b6c7cfafd93
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-05-29 16:24:16 +0300

    US-F01 Solution scaffold and shared building
    - implemented abstraction on top of IPipelineBehavior that acts as validation middleware

---
+++ src/Shared/LiveLearn.BuildingBlocks/ValidationBehavior.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace LiveLearn.BuildingBlocks;
+
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : Result
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!validators.Any()) return await next(cancellationToken);
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var errors = validators
+            .Select(v => v.Validate(context))
+            .SelectMany(r => r.Errors)
+            .Where(f => f != null)
+            .Select(f => f.ErrorMessage)
+            .ToArray();
+
+        if (errors.Length > 0)
+            return (TResponse)(object)Result.Failure(errors);
+
+        return await next(cancellationToken);
+    }
+}
