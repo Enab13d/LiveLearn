5ed05fd571ee95fb16573f8ccc408bfc0dfb6563
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-05-29 15:37:54 +0300

    US-F01 Solution scaffold and shared building
    - described generic repository interface to enforce a standartized set of CRUD operations

---
+++ src/Shared/LiveLearn.BuildingBlocks/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace LiveLearn.BuildingBlocks;
+
+public interface IRepository<TEntity, TId> where TEntity : Entity<TId>
+{
+    Task<TEntity?> GetByIdAsync(TId id, CancellationToken ct = default);
+
+    Task<IEnumerable<TEntity>> FindAsync(
+        System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate,
+        CancellationToken ct = default);
+
+    Task AddAsync(TEntity entity, CancellationToken ct = default);
+
+    void Update(TEntity entity);
+
+    void Delete(TEntity entity);
+}
