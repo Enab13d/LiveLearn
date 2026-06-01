deacf363230b29f793be661a777eb72876679fa6
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-01 19:06:04 +0300

    US-F01 Solution scaffold and shared building
    - implemented ClaimsPrincipalExtensions with helper method to get user id

---
+++ src/Shared/LiveLearn.BuildingBlocks/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace LiveLearn.BuildingBlocks;
+
+
+public static class ClaimsPrincipalExtensions
+{
+
+    public static string? GetUserId(this ClaimsPrincipal principal)
+    {
+
+        return principal.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+    }
+}
