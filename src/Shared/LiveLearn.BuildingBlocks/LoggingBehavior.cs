99a497c5eb8c1cfb88f6b646c88a80fda3583294
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-01 17:11:06 +0300

    US-F01 Solution scaffold and shared building
    - created generic LoggingBehavior

---
+++ src/Shared/LiveLearn.BuildingBlocks/LoggingBehavior.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace LiveLearn.BuildingBlocks;
+
+
+public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger) : IPipelineBehavior<TRequest, TResponse>
+where TRequest : IRequest<TResponse>
+where TResponse : Result
+{
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger = logger;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Handling Request: {RequestName} {@RequestData}", typeof(TRequest).Name, request);
+        var response = await next(cancellationToken);
+        if (response.IsSuccess)
+        {
+            _logger.LogInformation("Handled request: {RequestName} successfully", typeof(TRequest).Name);
+        }
+        else
+        {
+            _logger.LogWarning("Handled request: {RequestName} with errors {Errors}", typeof(TRequest).Name, string.Join(',', response.Errors ?? []));
+        }
+
+        return response;
+    }
+}
