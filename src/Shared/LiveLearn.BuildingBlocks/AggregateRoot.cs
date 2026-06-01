f8011375228017e75805cf5a28239f4b8e8b23cd
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-01 17:34:06 +0300

    US-F01 Solution scaffold and shared building
    - move domain events to AggregateRoot

--- src/Shared/LiveLearn.BuildingBlocks/AggregateRoot.cs
+++ src/Shared/LiveLearn.BuildingBlocks/AggregateRoot.cs
@@ -1,4 +1,19 @@
 ﻿namespace LiveLearn.BuildingBlocks;
 
 
-public abstract class AggregateRoot<TId> : Entity<TId> { }
+public abstract class AggregateRoot<TId> : Entity<TId>
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    protected void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
