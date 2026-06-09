cc0b41b79694d370f9fc43421d05ca418460c46c
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-09 16:33:35 +0300

    US-GW01 Request routing and JWT validation
    - Configure JWT Bearer authentication (Keycloak)
    - Implement only-authenticated authorization policy
    - apply only-authenticated policy for all non-public routes
    - configure rate-limiting middleware

---
+++ src/Gateway/LiveLearn.Gateway/Infrastructure/Authentication/JwtOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveLearn.Gateway.Infrastructure.Authentication;
+
+internal class JwtOptions
+{
+    public string Authority { get; set; } = string.Empty;
+    public string ValidIssuer { get; set; } = string.Empty;
+
+    public string ValidAudience { get; set; } = string.Empty;
+}
+
--- src/Gateway/LiveLearn.Gateway/Program.cs
+++ src/Gateway/LiveLearn.Gateway/Program.cs
@@ -1,18 +1,75 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Net;
+using System.Threading.RateLimiting;
+using LiveLearn.Gateway.Infrastructure.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.HttpOverrides;
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownIPNetworks.Clear();
+    options.KnownProxies.Clear();
+    options.KnownProxies.Add(IPAddress.Parse("172.20.0.1"));
+
+});
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        RateLimitPartition.GetSlidingWindowLimiter(
+                partitionKey: httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+                factory: partition => new SlidingWindowRateLimiterOptions
+                {
+                    PermitLimit = 100,
+                    Window = TimeSpan.FromMinutes(1),
+                    SegmentsPerWindow = 6,
+                    QueueLimit = 0,
+                }
+    )
+    );
+
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var jwtOptions = builder.Configuration.GetRequiredSection(nameof(JwtOptions)).Get<JwtOptions>()
+         ?? throw new InvalidOperationException("JWT options not defined");
+
+        options.Authority = jwtOptions.Authority;
+        options.MapInboundClaims = false;
+        options.RequireHttpsMetadata = !builder.Environment.IsDevelopment();
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtOptions.ValidIssuer,
+            ValidAudience = jwtOptions.ValidAudience,
+        };
+    });
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("onlyAuthenticated", policy => policy.RequireAuthenticatedUser());
+
 var app = builder.Build();
-app.MapReverseProxy();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+app.UseForwardedHeaders();
 app.UseHttpsRedirection();
+app.UseRateLimiter();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapReverseProxy();
+
 
 app.Run();
