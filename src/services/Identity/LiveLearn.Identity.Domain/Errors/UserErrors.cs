cb95635b853b4974c9ce58bab6cf7462541cf137
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-12 12:54:48 +0300

    US-ID01 User profile management
    - defined user errors

---
+++ src/services/Identity/LiveLearn.Identity.Domain/Errors/UserErrors.cs
@@ -0,0 +1,9 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Identity.Domain.Errors;
+
+public static class UserErrors
+{
+    public static readonly Error NotFound = Error.NotFound("User.NotFound", "User was not found.");
+    public static readonly Error EmailAlreadyTaken = Error.Conflict("User.EmailAlreadyTaken", "This email is already in  use.");
+}
