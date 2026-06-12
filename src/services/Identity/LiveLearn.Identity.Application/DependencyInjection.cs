f55b5293a62ffc3415f733308bc24cda94b23e2f
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-12 15:48:44 +0300

    US-ID01 User profile management
    -  registered Identity.Application MediatR pipeline with validation and logging behaviors

---
+++ src/services/Identity/LiveLearn.Identity.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using LiveLearn.BuildingBlocks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LiveLearn.Identity.Application;
+
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddIdentityApplication(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        return services;
+    }
+}
