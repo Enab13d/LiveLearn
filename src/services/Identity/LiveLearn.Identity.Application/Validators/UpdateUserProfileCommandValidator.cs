2e51221606cf7f2d755ac462b092bda57480aa89
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-12 15:31:31 +0300

    US-ID01 User profile management
    - implement validator for update user profile command

---
+++ src/services/Identity/LiveLearn.Identity.Application/Validators/UpdateUserProfileCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using LiveLearn.Identity.Application.Commands;
+
+namespace LiveLearn.Identity.Application.Validators;
+
+
+public class UpdateUserProfileCommandValidator : AbstractValidator<UpdateUserProfileCommand>
+{
+    public UpdateUserProfileCommandValidator()
+    {
+        RuleFor(user => user.DisplayName)
+            .NotEmpty().WithMessage("Display name is required");
+    }
+};
