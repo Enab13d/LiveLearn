82e617cb0fa7253d2ac2aad9147ee1557848b0eb
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-12 17:16:12 +0300

    US-ID01 User profile management
    - implemented user repository

---
+++ src/services/Identity/LiveLearn.Identity.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using LiveLearn.Identity.Application.Repositories;
+using LiveLearn.Identity.Domain.Entities;
+using LiveLearn.Identity.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Identity.Infrastructure.Repositories;
+
+
+internal sealed class UserRepository(IdentityDbContext dbContext) : IUserRepository
+{
+    public async Task AddAsync(User entity, CancellationToken ct = default)
+    {
+        await dbContext.Users.AddAsync(entity, ct);
+    }
+
+    public void Delete(User entity) => dbContext.Users.Remove(entity);
+
+    public async Task<IEnumerable<User>> FindAsync(Expression<Func<User, bool>> predicate, CancellationToken ct = default) =>
+        await dbContext.Users.Where(predicate).ToListAsync(ct);
+
+    public async Task<User?> GetByEmailAsync(string email, CancellationToken ct = default) =>
+        await dbContext.Users.FirstOrDefaultAsync(u => u.Email == email, ct);
+
+    public async Task<User?> GetByIdAsync(Guid id, CancellationToken ct = default) =>
+        await dbContext.Users.FindAsync([id], ct);
+
+    public void Update(User entity)
+    {
+        dbContext.Users.Update(entity);
+    }
+}
+
