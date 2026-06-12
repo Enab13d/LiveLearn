4dc19eeb38d9564a55ab5d23ec0fcbb647ba1cb9
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-12 17:28:03 +0300

    US-ID01 User profile management
    - implemented unit of work

---
+++ src/services/Identity/LiveLearn.Identity.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Identity.Infrastructure.Context;
+
+namespace LiveLearn.Identity.Infrastructure;
+
+
+internal sealed class UnitOfWork(IdentityDbContext dbContext) : IUnitOfWork
+{
+    public async Task<int> CommitAsync(CancellationToken ct = default)
+    {
+        return await dbContext.SaveChangesAsync(ct);
+    }
+}
