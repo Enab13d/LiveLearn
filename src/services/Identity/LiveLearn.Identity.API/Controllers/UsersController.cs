ddd2ac1d8aa8e9ee421bc92942051b830a9864ae
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-17 15:36:12 +0300

    US-ID01 User profile management
    - implement handler for auth data sync

--- src/services/Identity/LiveLearn.Identity.API/Controllers/UsersController.cs
+++ src/services/Identity/LiveLearn.Identity.API/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
-﻿using LiveLearn.Identity.API.Extensions;
+﻿using System.Security.Claims;
+using LiveLearn.Identity.API.Extensions;
+using LiveLearn.Identity.Application.Commands;
 using LiveLearn.Identity.Application.Queries;
+using LiveLearn.Identity.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,5 +19,28 @@ public sealed class UsersController(ISender mediator) : ControllerBase
     {
         var result = await mediator.Send(new GetUserProfileQuery(id), ct);
         return result.IsSuccess ? Ok(result.Value) : this.ToProblemResult(result.Errors);
+    }
+
+    [HttpPost("sync")]
+    public async Task<IActionResult> SyncAuthData(CancellationToken ct)
+    {
+        var claims = HttpContext.User;
+        var roleValue = claims.FindFirstValue("role");
+
+
+        if (!Enum.TryParse<Role>(roleValue, out var role))
+        {
+            return BadRequest($"Role {roleValue} is unsupported");
+        }
+        string sub = claims.FindFirstValue("sub") ?? "";
+        string firstName = claims.FindFirstValue("given_name") ?? "";
+        string lastName = claims.FindFirstValue("family_name") ?? "";
+        string email = claims.FindFirstValue("email") ?? "";
+        if (!Guid.TryParse(sub, out var id))
+        {
+            return BadRequest($"Sub claim {sub} is not a valid Guid");
+        }
+        var result = await mediator.Send(new ProvisionUserCommand(id, email, firstName, lastName, role), ct);
+        return result.IsSuccess ? NoContent() : this.ToProblemResult(result.Errors);
     }
 }
