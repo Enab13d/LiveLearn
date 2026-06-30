81d6a0f0866393051ece4e0aa8eadf699d7bbc35
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-06-30 17:58:58 +0300

    US-CA01 Tutors create and publish courses
    - implemented Section entity

---
+++ src/services/Catalog/LiveLearn.Catalog.Domain/Entities/Section.cs
@@ -0,0 +1,17 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Catalog.Domain.Entities;
+
+
+public sealed class Section : Entity<Guid>
+{
+    private Section() { }
+
+    public Guid CourseId { get; private set; }
+    public string Title { get; private set; } = string.Empty;
+    public int Order { get; private set; }
+    private readonly List<Lecture> _lectures  = [];
+
+    public IReadOnlyCollection<Lecture> Lectures => _lectures.AsReadOnly();
+
+}
