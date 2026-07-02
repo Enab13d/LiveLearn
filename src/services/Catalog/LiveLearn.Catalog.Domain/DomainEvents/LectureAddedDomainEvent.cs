113b2c152787ad359e52519b0f3cdf47a27e6b37
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-02 16:40:20 +0300

    US-CA01 Tutors create and publish courses - created domain event (lecture added)

---
+++ src/services/Catalog/LiveLearn.Catalog.Domain/DomainEvents/LectureAddedDomainEvent.cs
@@ -0,0 +1,6 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Catalog.Domain.DomainEvents;
+
+
+public sealed record LectureAddedDomainEvent(Guid CourseId, Guid SectionId, Guid LectureId) : IDomainEvent;
