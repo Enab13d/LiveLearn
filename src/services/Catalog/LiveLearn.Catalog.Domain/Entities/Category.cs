01ae372d6d8b3a3cf6ff4a53151649dda671b6cf
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-02 15:14:27 +0300

    US-CA01 Tutors create and publish courses
    - scaffold Category entity

---
+++ src/services/Catalog/LiveLearn.Catalog.Domain/Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Catalog.Domain.Entities;
+
+public sealed class Category : AggregateRoot<Guid>
+{
+    private Category() { }
+
+    public string Name { get; private set; } = string.Empty;
+
+    public string Slug { get; private set; } = string.Empty;
+
+    public static Category Create(Guid id, string name, string slug)
+    {
+        var category = new Category()
+        {
+            Id = id,
+            Name = name,
+            Slug = slug
+        };
+
+        return category;
+    }
+
+}
+
