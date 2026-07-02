d4729e94a569d19959538051240689a7bd2a037f
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-02 17:45:14 +0300

    US-CA01 Tutors create and publish courses -added lecture errors

---
+++ src/services/Catalog/LiveLearn.Catalog.Domain/Errors/LectureErrors.cs
@@ -0,0 +1,8 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Catalog.Domain.Errors;
+
+public static class LectureErrors
+{
+    public static readonly Error NotFound = Error.NotFound("Lecture.NotFound", "Lecture was not found");
+}
