e77e539545895497d00154c6f0f56b1e2cb92b45
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-03 14:23:21 +0300

    US-CA01 Tutors create and publish courses
    - scaffold category repository interface

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Domain.Entities;
+
+namespace LiveLearn.Catalog.Application.Repositories;
+
+
+public interface ICategoryRepository : IRepository<Category, Guid>
+{
+    
+}
