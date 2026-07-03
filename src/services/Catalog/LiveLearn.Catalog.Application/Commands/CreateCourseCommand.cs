d121837257111bba3b9cb19dbeb91f437d01d836
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-03 13:56:44 +0300

    US-CA01 Tutors create and publish courses
    - defined command for course creation

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Commands/CreateCourseCommand.cs
@@ -0,0 +1,7 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+
+namespace LiveLearn.Catalog.Application.Commands;
+
+
+public sealed record CreateCourseCommand(Guid categoryId, string title, string description, decimal price) : ICommand<CourseDto>;
---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Dto/CourseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace LiveLearn.Catalog.Application.Dto;
+
+public sealed record CourseDto(
+    Guid Id,
+    Guid TutorId,
+    Guid CategoryId,
+    string Title,
+    string Description,
+    string ThumbnailUrl,
+    decimal Price,
+    string Status
+);
