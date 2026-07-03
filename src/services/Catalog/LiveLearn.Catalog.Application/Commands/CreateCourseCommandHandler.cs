dc807a18f117c869d007cd62644f3e575c5827ea
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-03 14:36:10 +0300

    US-CA01 Tutors create and publish courses
    - implement CreateCourseCommandHandler

--- src/services/Catalog/LiveLearn.Catalog.Application/Commands/CreateCourseCommandHandler.cs
+++ src/services/Catalog/LiveLearn.Catalog.Application/Commands/CreateCourseCommandHandler.cs
@@ -1,12 +1,23 @@
 ﻿using LiveLearn.BuildingBlocks;
 using LiveLearn.Catalog.Application.Dto;
+using LiveLearn.Catalog.Application.Repositories;
+using LiveLearn.Catalog.Domain.Entities;
 
 namespace LiveLearn.Catalog.Application.Commands;
 
-internal sealed class CreateCourseCommandHandler() : ICommandHandler<CreateCourseCommand, CourseDto>
+internal sealed class CreateCourseCommandHandler(ICourseRepository courseRepository, ICategoryRepository categoryRepository, IUnitOfWork unitOfWork) : ICommandHandler<CreateCourseCommand, CourseDto>
 {
-    public Task<Result<CourseDto>> Handle(CreateCourseCommand request, CancellationToken ct)
+    public async Task<Result<CourseDto>> Handle(CreateCourseCommand request, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var category = await categoryRepository.GetByIdAsync(request.CategoryId, ct);
+        if(category is null)
+            return Result<CourseDto>.Failure();
+
+        var course = Course.Create(Guid.NewGuid(), request.TutorId, request.CategoryId, request.Title, request.Description, request.Price);
+        await courseRepository.AddAsync(course, ct);
+        await unitOfWork.CommitAsync(ct);
+        CourseDto dto = new(course.Id, course.TutorId, course.CategoryId, course.Title, course.Description, course.ThumbnailUrl, course.Price, course.Status.ToString());
+        return dto;
     }
+
 }
