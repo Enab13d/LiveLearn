560da4d4fce571db4a1a2be8b1924e6cc8f4f5b3
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-03 14:03:44 +0300

    US-CA01 Tutors create and publish courses
    - scaffold handler for create user command

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Commands/CreateCourseCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+
+namespace LiveLearn.Catalog.Application.Commands;
+
+internal sealed class CreateCourseCommandHandler() : ICommandHandler<CreateCourseCommand, CourseDto>
+{
+    public Task<Result<CourseDto>> Handle(CreateCourseCommand request, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
