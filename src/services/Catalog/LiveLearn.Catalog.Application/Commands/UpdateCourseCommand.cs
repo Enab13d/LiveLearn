f52d96f83ac8130d8e188227be1569b6895fb93c
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-03 14:46:27 +0300

    US-CA01 Tutors create and publish courses
    - added update course command

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Commands/UpdateCourseCommand.cs
@@ -0,0 +1,7 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+
+namespace LiveLearn.Catalog.Application.Commands;
+
+
+public sealed record UpdateCourseCommand(Guid CourseId, Guid CategoryId, Guid TutorId, string Title, string Description, decimal Price) : ICommand<CourseDto>;
