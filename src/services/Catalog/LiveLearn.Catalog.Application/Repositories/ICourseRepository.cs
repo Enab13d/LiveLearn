ba743c79861bccc809c171c65520ee38b0def4a2
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-03 14:06:58 +0300

    US-CA01 Tutors create and publish courses
    - scaffold course repository

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Repositories/ICourseRepository.cs
@@ -0,0 +1,10 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Domain.Entities;
+
+namespace LiveLearn.Catalog.Application.Repositories;
+
+
+public interface ICourseRepository : IRepository<Course, Guid>
+{
+    
+}
