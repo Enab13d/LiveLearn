bf4f4583bc4e62a816e2ab8cb094d9377bb71416
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-03 14:33:32 +0300

    US-CA01 Tutors create and publish courses
    - added CategoryErrors

---
+++ src/services/Catalog/LiveLearn.Catalog.Domain/Errors/CategoryErrors.cs
@@ -0,0 +1,8 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Catalog.Domain.Errors;
+
+public static class CategoryErrors
+{
+    public static readonly Error NotFound = Error.NotFound("Category.NotFound", "Category was not found");
+}
