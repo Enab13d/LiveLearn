ee532dae80693e256e7cb49cd8a56b03d3d4b40d
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-06 11:11:10 +0300

    US-CA01 Tutors create and publish courses
    - modify update course command (remove redundant properties)

--- src/services/Catalog/LiveLearn.Catalog.Application/Commands/UpdateCourseCommand.cs
+++ src/services/Catalog/LiveLearn.Catalog.Application/Commands/UpdateCourseCommand.cs
@@ -4,4 +4,4 @@ using LiveLearn.Catalog.Application.Dto;
 namespace LiveLearn.Catalog.Application.Commands;
 
 
-public sealed record UpdateCourseCommand(Guid CourseId, Guid CategoryId, Guid TutorId, string Title, string Description, decimal Price) : ICommand<CourseDto>;
+public sealed record UpdateCourseCommand(Guid CourseId, string Title, string Description, decimal Price) : ICommand<CourseDto>;
