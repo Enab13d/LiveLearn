0e41f15f17836555acf5cf2c82b155cef287b2ae
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-06 17:55:32 +0300

    US-CA01 Tutors create and publish courses
    - added business logic in update course command handler

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Commands/UpdateCourseCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+using LiveLearn.Catalog.Application.Repositories;
+using LiveLearn.Catalog.Domain.Errors;
+
+
+namespace LiveLearn.Catalog.Application.Commands;
+
+
+public sealed class UpdateCourseCommandHandler(ICourseRepository courseRepository, IUnitOfWork unitOfWork) : ICommandHandler<UpdateCourseCommand, CourseDto>
+{
+    public async Task<Result<CourseDto>> Handle(UpdateCourseCommand request, CancellationToken ct)
+    {
+        var course = await courseRepository.GetByIdAsync(request.CourseId, ct);
+
+        if (course is null)
+            return Result<CourseDto>.Failure(CourseErrors.NotFound);
+
+        if (course.TutorId != request.TutorId)
+            return Result<CourseDto>.Failure(CourseErrors.Forbidden);
+
+        course.Update(request.Title, request.Description, request.Price, request.CategoryId);
+
+        await unitOfWork.CommitAsync(ct);
+
+        return new CourseDto(
+            Id: course.Id,
+            TutorId: course.TutorId,
+            CategoryId: course.CategoryId,
+            Title: course.Title,
+            Description: course.Description,
+            ThumbnailUrl: course.ThumbnailUrl,
+            Price: course.Price,
+            Status: course.Status.ToString()
+        );
+    }
+}
