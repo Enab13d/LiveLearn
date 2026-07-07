f81f47090e315e9126e28d3f9dfce79517321515
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-07 16:39:39 +0300

    US-CA01 Tutors create and publish courses
    - implement add section feature pt. 2

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Commands/AddSectionCommand.cs
@@ -0,0 +1,9 @@
+﻿using LiveLearn.BuildingBlocks;
+
+
+namespace LiveLearn.Catalog.Application.Commands;
+
+public sealed record AddSectionCommand(Guid SectionId, Guid CourseId, Guid TutorId, string Title, int Order) : ICommand<Guid>;
+
+
+
