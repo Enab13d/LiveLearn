bf60c4647d1cc5fc5fdba9b2dc7db95cd0f6043c
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 11:17:21 +0300

    feat (BuildingBlocks)
    - added PagedResult

---
+++ src/Shared/LiveLearn.BuildingBlocks/PagedResult.cs
@@ -0,0 +1,12 @@
+﻿namespace LiveLearn.BuildingBlocks;
+
+public sealed class PagedResult<T>(List<T> items, int count, int pageNumber, int pageSize)
+{
+    public IReadOnlyList<T> Items { get; init; } = items;
+    public int CurrentPage { get; init; } = pageNumber;
+    public int PageSize { get; init; } = pageSize;
+    public int TotalCount { get; init; } = count;
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
+};
