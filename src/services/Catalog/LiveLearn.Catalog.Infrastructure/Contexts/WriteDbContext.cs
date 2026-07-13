a069df4db8fcd497b7b38e2a48e096cdd343cbd9
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 17:46:23 +0300

    US-CA01 Tutors create and publish courses
    - scaffolded WriteDbContext

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Contexts/WriteDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LiveLearn.Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Catalog.Infrastructure.Contexts;
+
+
+public sealed class WriteDbContext(DbContextOptions<WriteDbContext> options) : DbContext(options)
+{
+    public DbSet<Course> Courses { get; set; } = null!;
+
+    public DbSet<Category> Categories { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(WriteDbContext).Assembly,
+            a => a.AssemblyQualifiedName?.Contains("EntityConfigurations.Write") == true);
+    }
+
+}
