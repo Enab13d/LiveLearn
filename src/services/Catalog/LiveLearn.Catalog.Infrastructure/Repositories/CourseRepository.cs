2e17e0f68d71fac15e371df3657131cb0a8d7a34
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 19:13:41 +0300

    US-CA01 Tutors create and publish courses - implemented CourseRepository

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Repositories/CourseRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using LiveLearn.Catalog.Application.Repositories;
+using LiveLearn.Catalog.Domain.Entities;
+using LiveLearn.Catalog.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Catalog.Infrastructure.Repositories;
+
+
+internal sealed class CourseRepository(WriteDbContext dbContext) : ICourseRepository
+{
+    public async Task AddAsync(Course entity, CancellationToken ct = default)
+    {
+        await dbContext.Courses.AddAsync(entity, ct);
+    }
+
+    public void Delete(Course entity)
+    {
+        dbContext.Courses.Remove(entity);
+    }
+
+    public async Task<IEnumerable<Course>> FindAsync(Expression<Func<Course, bool>> predicate, CancellationToken ct = default)
+    {
+        return await dbContext.Courses.Where(predicate).ToListAsync(ct);
+    }
+
+    public async Task<Course?> GetByIdAsync(Guid id, CancellationToken ct = default)
+    {
+        return await dbContext.Courses
+            .Include(e => e.Sections)
+            .ThenInclude(e => e.Lectures)
+            .AsSplitQuery()
+            .FirstOrDefaultAsync(e => e.Id == id, ct);
+    }
+
+    public void Update(Course entity)
+    {
+        dbContext.Courses.Update(entity);
+    }
+}
