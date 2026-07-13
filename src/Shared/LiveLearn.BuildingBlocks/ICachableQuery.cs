234317e4eda9dc5d10fd85225fb2f7cd5c57588d
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 13:09:37 +0300

    US-CA01 Tutors create and publish courses
    - implemented caching behaviour (pt. 1)

---
+++ src/Shared/LiveLearn.BuildingBlocks/ICachableQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace LiveLearn.BuildingBlocks;
+
+public interface ICachableQuery
+{
+    bool BypassCache { get; }
+    string CacheKey { get; }
+
+    TimeSpan? SlidingExpiration { get; }
+
+    TimeSpan? AbsoluteExpiration { get; }
+}
