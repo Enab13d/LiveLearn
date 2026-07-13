aae50ca4af96f0d89db6197b2ac7bcba79c6f2ac
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 17:54:04 +0300

    US-CA01 Tutors create and publish courses
    - scaffolded ReadDbContext

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Contexts/ReadDbContext.cs
@@ -0,0 +1,22 @@
+﻿using LiveLearn.Catalog.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Catalog.Infrastructure.Contexts;
+
+
+internal sealed class ReadDbContext(DbContextOptions<ReadDbContext> options) : DbContext(options)
+{
+    public DbSet<CourseReadModel> Courses { get; set; } = null!;
+    public DbSet<SectionReadModel> Sections { get; set; } = null!;
+    public DbSet<LectureReadModel> Lectures { get; set; } = null!;
+    public DbSet<CategoryReadModel> Categories { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ReadDbContext).Assembly,
+            a => a.AssemblyQualifiedName?.Contains("EntityConfigurations.Read") == true
+        );
+    }
+
+}
