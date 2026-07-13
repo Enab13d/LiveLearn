4df852f5849723244f0e7cc3e6f9be4355a02b9f
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 19:24:32 +0300

    US-CA01 Tutors create and publish courses
    - implemented UnitOfWork

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Infrastructure.Contexts;
+
+namespace LiveLearn.Catalog.Infrastructure;
+
+
+internal sealed class UnitOfWork(WriteDbContext dbContext) : IUnitOfWork
+{
+    public async Task<int> CommitAsync(CancellationToken ct = default)
+    {
+        return await dbContext.SaveChangesAsync(ct);
+    }
+}
