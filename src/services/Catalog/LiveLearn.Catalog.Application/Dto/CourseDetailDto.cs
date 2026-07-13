055bc626ceca5d8f40c87b31af3cd4ed21006cf0
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 10:50:09 +0300

    US-CA01 Tutors create and publish courses
    - created CourseDetailDto

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Dto/CourseDetailDto.cs
@@ -0,0 +1,14 @@
+﻿namespace LiveLearn.Catalog.Application.Dto;
+
+public sealed record CourseDetailDto(
+    Guid Id,
+    Guid CategoryId,
+    string CategoryName,
+    Guid TutorId,
+    string Title,
+    string Description,
+    string ThumbnailUrl,
+    decimal Price,
+    string Status,
+    IReadOnlyList<SectionDto> Sections
+);
