8781847fb6cea243857ec3b8f785ea18de893f9b
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 11:41:22 +0300

    US-CA01 Tutors create and publish courses
    - created GetCoursesByTutorQuery

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Queries/GetCoursesByTutorQuery.cs
@@ -0,0 +1,10 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+
+namespace LiveLearn.Catalog.Application.Queries;
+
+public sealed record GetCoursesByTutorQuery(
+    Guid TutorId, 
+    int PageNumber, 
+    int PageSize
+) : IQuery<PagedResult<CourseDto>>;
