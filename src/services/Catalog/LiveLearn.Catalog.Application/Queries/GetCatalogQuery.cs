f04c8cec48f0f86169c3172344a3a27c68ad35b1
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 11:31:18 +0300

    US-CA01 Tutors create and publish courses
    - created GetCatalogQuery

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Queries/GetCatalogQuery.cs
@@ -0,0 +1,12 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+
+namespace LiveLearn.Catalog.Application.Queries;
+
+public sealed record GetCatalogQuery(
+    int PageNumber,
+    int PageSize,
+    Guid? CategoryId = null,
+    Guid? TutorId = null,
+    decimal? MaxPrice = null
+) : IQuery<PagedResult<CatalogItemDto>>;
