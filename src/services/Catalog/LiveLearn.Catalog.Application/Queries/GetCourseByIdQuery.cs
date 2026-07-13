4466e98638f4c89d1f159fd36b625172561ed31f
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 10:54:01 +0300

    US-CA01 Tutors create and publish courses
    - created GetCourseByIdQuery

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Queries/GetCourseByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+
+namespace LiveLearn.Catalog.Application.Queries;
+
+
+public sealed record GetCourseByIdQuery(Guid CourseId): IQuery<CourseDetailDto>;
