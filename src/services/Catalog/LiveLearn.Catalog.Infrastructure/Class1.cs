7e91c4d5739e6c8b23abaa532dca6d8914975eb0
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-13 15:31:53 +0300

    US-CA01 Tutors create and publish courses
    - created read model for Course

--- src/services/Catalog/LiveLearn.Catalog.Infrastructure/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LiveLearn.Catalog.Infrastructure;
-
-public class Class1
-{
-
-}
---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Models/CourseReadModel.cs
@@ -0,0 +1,22 @@
+﻿namespace LiveLearn.Catalog.Infrastructure.Models;
+
+internal class CourseReadModel
+{
+    public Guid Id { get; set; }
+    public Guid CategoryId { get; set; }
+    public Guid TutorId { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string ThumbnailUrl { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+
+    public CategoryReadModel Category { get; set; } = default!;
+
+    public List<SectionReadModel> Sections { get; set; } = [];
+}
