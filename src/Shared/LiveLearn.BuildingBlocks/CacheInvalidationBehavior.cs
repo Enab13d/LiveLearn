6a14e09388110255e8e3552fa598e9723a4ca8e2
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-14 15:40:40 +0300

    US-CA01 Tutors create and publish courses
    - implemented cache invalidation behavior

---
+++ src/Shared/LiveLearn.BuildingBlocks/CacheInvalidationBehavior.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace LiveLearn.BuildingBlocks;
+
+public sealed class CacheInvalidationBehavior<TRequest, TResponse>(ICacheService cache): IPipelineBehavior<TRequest, TResponse>
+where TRequest : IRequest<TResponse>, ICacheInvalidationCommand
+where TResponse : Result
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken ct)
+    {
+        var result = await next(ct);
+        if (result.IsSuccess)
+            foreach (var key in request.CacheKeys)
+                await cache.RemoveAsync(key.ToLower(), ct);
+
+        return result;
+    }
+}
