08448fc0487bb4ef71db6c68735d8c33ab3c529a
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-14 12:36:34 +0300

    US-CA01 Tutors create and publish courses
    - implemented inter-process communication (pt. 2)

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Messaging/MassTransitEventBus.cs
@@ -0,0 +1,12 @@
+﻿using LiveLearn.BuildingBlocks;
+using MassTransit;
+
+namespace LiveLearn.Catalog.Infrastructure.Messaging;
+
+internal sealed class MassTransitEventBus(IPublishEndpoint publishEndpoint) : IEventBus
+{
+    public async Task PublishAsync<T>(T message, CancellationToken ct = default) where T : class
+    {
+        await publishEndpoint.Publish<T>(message, ct);
+    }
+}
