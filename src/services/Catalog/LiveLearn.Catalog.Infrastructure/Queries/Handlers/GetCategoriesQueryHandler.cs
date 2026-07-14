1321dcf40412d58fe27eaab49eeefe4a64cb1f36
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-14 18:00:49 +0300

    US-CA01 Tutors create and publish courses
    - added GetCategoriesQueryHandler

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Queries/Handlers/GetCategoriesQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+using LiveLearn.Catalog.Application.Queries;
+using LiveLearn.Catalog.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Catalog.Infrastructure.Queries.Handlers;
+
+internal sealed class GetCategoriesQueryHandler(ReadDbContext dbContext) : IQueryHandler<GetCategoriesQuery, IReadOnlyList<CategoryDto>>
+{
+    public async Task<Result<IReadOnlyList<CategoryDto>>> Handle(GetCategoriesQuery request, CancellationToken ct)
+    {
+        return await dbContext.Categories.Select(e => new CategoryDto(e.Id, e.Name, e.Slug)).ToListAsync(ct);
+    }
+}
