bb150aff8b1e472843122bd546a94043609908e0
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-14 13:46:56 +0300

    US-CA01 Tutors create and publish courses
    - implemented inter-process communication (pt. 3)

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Configuration/RabbitMQOptions.cs
@@ -0,0 +1,13 @@
+﻿
+namespace LiveLearn.Catalog.Infrastructure.Configuration;
+
+internal class RabbitMQOptions
+{
+    public string Host { get; set; } = string.Empty;
+
+    public string VirtualHost { get; set; } = "/";
+    public string Username { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
+
---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,47 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Infrastructure.Configuration;
+using LiveLearn.Catalog.Infrastructure.Contexts;
+using LiveLearn.Catalog.Infrastructure.Messaging;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LiveLearn.Catalog.Infrastructure;
+
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddCatalogInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.Configure<RabbitMQOptions>(configuration.GetSection(nameof(RabbitMQOptions)));
+
+        var rabbitMQOptions = configuration.GetRequiredSection(nameof(RabbitMQOptions)).Get<RabbitMQOptions>()
+            ?? throw new InvalidOperationException("RabbitMQOptions options not defined");
+
+        services.AddScoped<IEventBus, MassTransitEventBus>();
+        services.AddMassTransit(x =>
+        {
+            x.SetKebabCaseEndpointNameFormatter();
+            x.UsingRabbitMq((context, cfg) =>
+            {
+
+                cfg.Host(rabbitMQOptions.Host, rabbitMQOptions.VirtualHost, h =>
+                {
+                    h.Username(rabbitMQOptions.Username);
+                    h.Password(rabbitMQOptions.Password);
+                });
+                cfg.ConfigureEndpoints(context);
+            });
+
+            x.AddEntityFrameworkOutbox<WriteDbContext>(o =>
+            {
+                o.UsePostgres();
+
+                o.UseBusOutbox();
+            });
+        });
+
+        return services;
+    }
+}
