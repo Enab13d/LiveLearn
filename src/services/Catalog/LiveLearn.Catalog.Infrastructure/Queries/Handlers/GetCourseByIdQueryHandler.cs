dde7f3ab349702a4c835cbac737fcd3210af662f
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-14 18:29:16 +0300

    US-CA01 Tutors create and publish courses
    - implemented GetCourseByIdQueryHandler

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Queries/Handlers/GetCourseByIdQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+using LiveLearn.Catalog.Application.Queries;
+using LiveLearn.Catalog.Domain.Errors;
+using LiveLearn.Catalog.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Catalog.Infrastructure.Queries.Handlers;
+
+internal sealed class GetCourseByIdQueryHandler(ReadDbContext dbContext) : IQueryHandler<GetCourseByIdQuery, CourseDetailDto>
+{
+    public async Task<Result<CourseDetailDto>> Handle(GetCourseByIdQuery request, CancellationToken ct)
+    {
+
+        var courseDetails = await dbContext.Courses
+        .Where(e => e.Id == request.CourseId)
+        .Select(e => new CourseDetailDto(e.Id,
+            e.CategoryId,
+            e.Category.Name,
+            e.TutorId,
+            e.Title,
+            e.Description,
+            e.ThumbnailUrl,
+            e.Price,
+            e.Status,
+            e.Sections
+            .Select(e => new SectionDto(
+                e.Id,
+                e.Title,
+                e.Order,
+                e.Lectures
+                .Select(e => new LectureDto(
+                    e.Id, e.Title, e.Type, e.Order, e.DurationInSeconds))
+                .ToList()
+                ))
+            .ToList()
+            )
+        ).FirstOrDefaultAsync(ct);
+
+        if (courseDetails is null) return Result<CourseDetailDto>.Failure(CourseErrors.NotFound);
+        return courseDetails;
+
+
+    }
+}
