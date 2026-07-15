b88d14e616feec170cf1669d347c7acdd9186c53
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-15 11:13:33 +0300

    US-CA01 Tutors create and publish courses
    - added DI extension for Catalog Application

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using LiveLearn.BuildingBlocks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LiveLearn.Catalog.Application;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddCatalogApplication(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        return services;
+    }
+}
