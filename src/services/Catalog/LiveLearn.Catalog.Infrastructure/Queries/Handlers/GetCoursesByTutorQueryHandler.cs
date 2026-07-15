f33f38daadb42e2a57f8544b1b37c58228c6a63e
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-15 09:59:18 +0300

    US-CA01 Tutors create and publish courses
    - implemented GetCoursesByTutorQueryHandler

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Queries/Handlers/GetCoursesByTutorQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+using LiveLearn.Catalog.Application.Queries;
+using LiveLearn.Catalog.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Catalog.Infrastructure.Queries.Handlers;
+
+
+internal sealed class GetCoursesByTutorQueryHandler(ReadDbContext dbContext)
+: IQueryHandler<GetCoursesByTutorQuery, PagedResult<CourseDto>>
+{
+    public async Task<Result<PagedResult<CourseDto>>> Handle(GetCoursesByTutorQuery request, CancellationToken ct)
+    {
+        var query = dbContext.Courses
+            .Where(c => c.TutorId == request.TutorId)
+            .Select(c => new CourseDto(c.Id, c.TutorId, c.CategoryId, c.Title, c.Description, c.ThumbnailUrl, c.Price, c.Status));
+
+        int totalCount = await query.CountAsync(ct);
+
+        if (request.PageNumber > 0 && request.PageSize > 0)
+            query = query
+            .Skip((request.PageNumber - 1) * request.PageSize)
+            .Take(request.PageSize);
+
+        var courses = await query.ToListAsync(ct);
+
+        return new PagedResult<CourseDto>(courses, totalCount, request.PageNumber, request.PageSize);
+
+
+    }
+}
