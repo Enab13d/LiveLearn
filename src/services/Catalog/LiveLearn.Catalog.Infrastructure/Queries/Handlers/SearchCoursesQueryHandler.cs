00992ee2e7f1697afa45c93660972ba1f649822b
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-15 11:01:45 +0300

    US-CA01 Tutors create and publish courses
    - implemented SearchCoursesQueryHandler

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Queries/Handlers/SearchCoursesQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+using LiveLearn.Catalog.Application.Queries;
+using LiveLearn.Catalog.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using NpgsqlTypes;
+
+namespace LiveLearn.Catalog.Infrastructure.Queries.Handlers;
+
+
+internal sealed class SearchCoursesQueryHandler(ReadDbContext dbContext) : IQueryHandler<SearchCoursesQuery, PagedResult<CatalogItemDto>>
+{
+    public async Task<Result<PagedResult<CatalogItemDto>>> Handle(SearchCoursesQuery request, CancellationToken ct)
+    {
+        IQueryable<CatalogItemDto> query = dbContext.Courses
+            .Where(e => EF.Property<NpgsqlTsVector>(e, "SearchVector")
+                .Matches(EF.Functions.WebSearchToTsQuery("english", request.Query)))
+            .OrderByDescending(e => EF.Property<NpgsqlTsVector>(e, "SearchVector")
+                .Rank(EF.Functions.WebSearchToTsQuery("english", request.Query)))
+            .Select(e => new CatalogItemDto(e.Id, e.TutorId, e.CategoryId, e.Title, e.ThumbnailUrl, e.Price));
+
+        var totalCount = await query.CountAsync(ct);
+
+        if (request.PageNumber > 0 && request.PageSize > 0)
+            query = query
+                .Skip((request.PageNumber - 1) * request.PageSize)
+                .Take(request.PageSize);
+
+        var catalogItems = await query.ToListAsync(ct);
+
+        return new PagedResult<CatalogItemDto>(catalogItems, totalCount, request.PageNumber, request.PageSize);
+    }
+}
