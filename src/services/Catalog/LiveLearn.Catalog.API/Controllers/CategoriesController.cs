4bca1d0177086a913882e0b987a371ca7f670ff8
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-16 17:36:06 +0300

    US-CA01 Tutors create and publish courses
    - implemented CategoriesController

---
+++ src/services/Catalog/LiveLearn.Catalog.API/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using LiveLearn.Catalog.API.Extensions;
+using LiveLearn.Catalog.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveLearn.Catalog.API.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public sealed class CategoriesController(ISender mediator) : ControllerBase
+{
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IActionResult> GetCategories(CancellationToken ct)
+    {
+        var result = await mediator.Send(new GetCategoriesQuery(), ct);
+        return result.IsSuccess ? Ok(result.Value) : this.ToProblemResult(result.Errors);
+    }
+}
