a7e9757df65a50af07a543507d730400720a0d3a
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-21 17:50:06 +0300

    US-CA01 Tutors create and publish courses
    - configured Serilog in Catalog

--- src/services/Catalog/LiveLearn.Catalog.API/Program.cs
+++ src/services/Catalog/LiveLearn.Catalog.API/Program.cs
@@ -1,18 +1,52 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Serilog;
+using Serilog.Formatting.Compact;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, config) =>
+    {
+        config
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("Service", "catalog");
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            config.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{Service}] {SourceContext}: {Message:lj}{NewLine}{Exception}");
+        }
+        else
+        {
+            config.WriteTo.Console(new CompactJsonFormatter());
+        }
+    });
+    // Add services to the container.
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+
+    var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+    app.UseSerilogRequestLogging();
+    app.UseHttpsRedirection();
+
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
