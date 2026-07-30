4d3d03ff5a70affdc13785fd22dd96503b363385
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-30 15:21:01 +0300

    US-CA02 Redis caching for the catalog
    - worked on hybrid caching approach implementation pt. 2

--- src/Shared/LiveLearn.BuildingBlocks/CacheEntryOptions.cs
+++ src/Shared/LiveLearn.BuildingBlocks/CacheEntryOptions.cs
@@ -2,6 +2,6 @@
 
 public sealed class CacheEntryOptions
 {
-    public TimeSpan? SlidingExpiration { get; init; }
-    public TimeSpan? AbsoluteExpiration { get; init; }
+    public TimeSpan? LocalCacheExpiration { get; init; }
+    public TimeSpan? Expiration { get; init; }
 }
---
+++ src/Shared/LiveLearn.BuildingBlocks/ICacheableQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace LiveLearn.BuildingBlocks;
+
+public interface ICacheableQuery
+{
+    bool BypassCache { get; }
+    string CacheKey { get; }
+    CacheEntryOptions? Options { get; }
+    IEnumerable<string> Tags { get; }
+}
