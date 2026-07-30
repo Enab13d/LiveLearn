7344ac491c52c842341a66a2313fa8d29378fcdf
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-07-30 15:39:00 +0300

    US-CA02 Redis caching for the catalog
    - implemented cache invalidation on PublishCourseCommand

--- src/services/Catalog/LiveLearn.Catalog.Application/Commands/PublishCourseCommand.cs
+++ src/services/Catalog/LiveLearn.Catalog.Application/Commands/PublishCourseCommand.cs
@@ -2,4 +2,7 @@
 
 namespace LiveLearn.Catalog.Application.Commands;
 
-public sealed record PublishCourseCommand(Guid CourseId, Guid TutorId) : ICommand;
+public sealed record PublishCourseCommand(Guid CourseId, Guid TutorId) : ICommand, ICacheInvalidationCommand
+{
+    public IEnumerable<string> Tags => ["catalog"];
+}
