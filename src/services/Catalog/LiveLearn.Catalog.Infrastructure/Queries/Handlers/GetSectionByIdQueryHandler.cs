46518ce8425e39246e40c063493f1bb14be06266
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-06 15:02:46 +0300

    US-CA01 Tutors create and publish courses
    - worked on Sections CRUD pt. 13

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Queries/Handlers/GetSectionByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Application.Dto;
+using LiveLearn.Catalog.Application.Queries;
+using LiveLearn.Catalog.Domain.Errors;
+using LiveLearn.Catalog.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Catalog.Infrastructure.Queries.Handlers;
+
+internal sealed class GetSectionByIdQueryHandler(ReadDbContext dbContext) : IQueryHandler<GetSectionByIdQuery, SectionDto>
+{
+    public async Task<Result<SectionDto>> Handle(GetSectionByIdQuery request, CancellationToken ct)
+    {
+        var section = await dbContext.Sections
+            .Where(e => e.Id == request.SectionId && e.CourseId == request.CourseId)
+            .Select(e => new SectionDto(
+                e.Id,
+                e.Title,
+                e.Order,
+                e.Lectures
+                    .Select(l => new LectureDto(l.Id, l.Title, l.Type, l.Order, l.DurationInSeconds))
+                    .ToList()
+                ))
+            .AsSplitQuery()
+            .FirstOrDefaultAsync(ct);
+
+        if (section is null) return Result<SectionDto>.Failure(CourseErrors.SectionNotFound);
+        return section;
+    }
+}
