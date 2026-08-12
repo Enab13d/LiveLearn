4dc456061604e1989069cde3da8228dd4f230ba0
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-12 21:47:28 +0300

    US-CA01 Tutors create and publish courses
    - improved reordering for sections/lectures pt. 8

--- src/services/Catalog/LiveLearn.Catalog.Application/Validators/UpdateSectionOrderCommandValidator.cs
+++ src/services/Catalog/LiveLearn.Catalog.Application/Validators/UpdateSectionOrderCommandValidator.cs
@@ -9,6 +9,19 @@ public sealed class UpdateSectionOrderCommandValidator : AbstractValidator<Updat
     {
         RuleFor(x => x.CourseId).NotEmpty();
         RuleFor(x => x.SectionId).NotEmpty();
-        RuleFor(x => x.Order).GreaterThanOrEqualTo(1);
+        RuleFor(x => x)
+            .Must(x => x.PreviousSectionId is not null || x.NextSectionId is not null)
+            .WithMessage("At least one of PreviousSectionId or NextSectionId is required.");
+        RuleFor(x => x.PreviousSectionId)
+            .NotEqual(x => x.SectionId)
+            .When(x => x.PreviousSectionId is not null)
+            .WithMessage("PreviousSectionId cannot equal SectionId.");
+        RuleFor(x => x.NextSectionId)
+            .NotEqual(x => x.SectionId)
+            .When(x => x.NextSectionId is not null)
+            .WithMessage("NextSectionId cannot equal SectionId.");
+        RuleFor(x => x)
+            .Must(x => x.PreviousSectionId is null || x.NextSectionId is null || x.PreviousSectionId != x.NextSectionId)
+            .WithMessage("PreviousSectionId and NextSectionId cannot be the same.");
     }
 }
