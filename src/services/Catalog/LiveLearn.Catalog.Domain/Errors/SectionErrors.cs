420202435aac764e14c462bd6463981d25b0bd01
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-19 19:21:13 +0300

    US-CA03 Task assignment to sections
    - added section errors (task-related)

---
+++ src/services/Catalog/LiveLearn.Catalog.Domain/Errors/SectionErrors.cs
@@ -0,0 +1,10 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Catalog.Domain.Errors;
+
+public static class SectionErrors
+{
+    public static readonly Error TaskNotFound = Error.NotFound("Section.TaskNotFound", "Section task was not found");
+
+    public static readonly Error TaskAlreadyAssigned = Error.Conflict("Section.TaskAlreadyAssigned", "This task is already assigned to this section");
+}
