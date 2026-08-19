0db955fbdac8dd80ce05c53d233620d2c9ccc80c
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-19 18:51:42 +0300

    US-CA03 Task assignment to sections
    - scaffold SectionTask entity

---
+++ src/services/Catalog/LiveLearn.Catalog.Domain/Entities/SectionTask.cs
@@ -0,0 +1,26 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Catalog.Domain.Entities;
+
+
+public sealed class SectionTask : Entity<Guid>
+{
+
+    public Guid SectionId { get; private set; }
+
+    public Guid CourseId { get; private set; }
+    public Guid TaskId { get; private set; }
+
+    public string TaskType { get; private set; } = string.Empty;
+    public DateTimeOffset AssignedAt { get; private set; }
+    private SectionTask() { }
+    internal SectionTask(Guid sectionId, Guid courseId, Guid taskId, string taskType)
+    {
+
+        SectionId = sectionId;
+        CourseId = courseId;
+        TaskId = taskId;
+        TaskType = taskType;
+    }
+
+}
