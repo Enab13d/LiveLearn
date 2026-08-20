b46ed19bed265bb4c5cd10d5e096c96b34355c68
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-20 11:41:51 +0300

    US-CA03 Task assignment to sections
    - added migrations (task-replica)

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Migrations/20260820083756_TaskReplica.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiveLearn.Catalog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskReplica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SectionTask",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SectionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TaskId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TaskType = table.Column<string>(type: "text", nullable: false),
+                    AssignedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SectionTask", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SectionTask_sections_SectionId",
+                        column: x => x.SectionId,
+                        principalTable: "sections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "task_replicas",
+                columns: table => new
+                {
+                    TaskId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TutorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TaskType = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_task_replicas", x => x.TaskId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectionTask_SectionId",
+                table: "SectionTask",
+                column: "SectionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SectionTask");
+
+            migrationBuilder.DropTable(
+                name: "task_replicas");
+        }
+    }
+}
