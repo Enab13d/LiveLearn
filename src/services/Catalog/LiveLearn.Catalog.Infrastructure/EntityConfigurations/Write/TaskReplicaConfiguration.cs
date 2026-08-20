aae6b472aff72edbdc23987e49db2784cd1cdd59
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-20 11:23:01 +0300

    US-CA03 Task assignment to sections
    - added entity write configuration for TaskReplica

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/EntityConfigurations/Write/TaskReplicaConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using LiveLearn.Catalog.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LiveLearn.Catalog.Infrastructure.EntityConfigurations.Write;
+
+internal sealed class TaskReplicaConfiguration : IEntityTypeConfiguration<TaskReplica>
+{
+    public void Configure(EntityTypeBuilder<TaskReplica> builder)
+    {
+        builder.ToTable("task_replicas");
+        builder.HasKey(e => e.TaskId);
+        builder.Property(e => e.TaskId).ValueGeneratedNever();
+        builder.Property(e => e.TutorId).IsRequired();
+        builder.Property(e => e.TaskType).IsRequired().HasMaxLength(16);
+    }
+}
