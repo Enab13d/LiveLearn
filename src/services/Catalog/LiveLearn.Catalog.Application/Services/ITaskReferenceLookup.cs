5c75c008067c0f8890ddf2d69da715643986d262
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-20 11:05:41 +0300

    US-CA03 Task assignment to sections
    - added abstraction over task reference lookup

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Services/ITaskReferenceLookup.cs
@@ -0,0 +1,9 @@
+﻿using LiveLearn.Catalog.Application.Dto;
+
+namespace LiveLearn.Catalog.Application.Services;
+
+
+public interface ITaskReferenceLookup
+{
+    public Task<TaskReferenceDto?> GetByTaskIdAsync(Guid taskId, CancellationToken ct = default);
+}
