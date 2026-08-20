8cfa9ae43d7ebc9ef42f692d2f7b1ac181462d60
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-20 11:12:01 +0300

    US-CA03 Task assignment to sections
    - added TaskReplica model (represents record from replica)

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Models/TaskReplica.cs
@@ -0,0 +1,11 @@
+﻿namespace LiveLearn.Catalog.Infrastructure.Models;
+
+
+internal sealed class TaskReplica
+{
+    public Guid TaskId { get; set; }
+
+    public Guid TutorId { get; set; }
+
+    public string TaskType { get; set; } = string.Empty;
+}
