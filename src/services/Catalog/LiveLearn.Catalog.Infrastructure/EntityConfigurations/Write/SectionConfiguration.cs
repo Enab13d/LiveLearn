3ae3ee10b5925d3f0088922cd652f9baa2ce185d
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-21 15:53:10 +0300

    US-CA03 Task assignment to sections
    - update section entity type configuration (write)

--- src/services/Catalog/LiveLearn.Catalog.Infrastructure/EntityConfigurations/Write/SectionConfiguration.cs
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/EntityConfigurations/Write/SectionConfiguration.cs
@@ -14,6 +14,10 @@ internal sealed class SectionConfiguration : IEntityTypeConfiguration<Section>
         builder.Property(e => e.Id).ValueGeneratedNever();
         builder.Property(e => e.Title).IsRequired().HasMaxLength(128);
         builder.HasMany(e => e.Lectures).WithOne().HasForeignKey(e => e.SectionId);
+        builder.HasMany(e => e.SectionTasks)
+            .WithOne()
+            .HasForeignKey(e => e.SectionId)
+            .OnDelete(DeleteBehavior.Cascade);
 
     }
 }
