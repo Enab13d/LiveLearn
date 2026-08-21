97ad412c91094a285dde01a1e0668e32eb6e4acd
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-21 16:06:40 +0300

    US-CA03 Task assignment to sections
    - scaffollded abstraction over MediatR INotificationHandler for domain event handlers

---
+++ src/Shared/LiveLearn.BuildingBlocks/IDomainEventHandler.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace LiveLearn.BuildingBlocks;
+
+public interface IDomainEventHandler<in TDomainEvent> : INotificationHandler<TDomainEvent>
+where TDomainEvent : IDomainEvent
+{ }
