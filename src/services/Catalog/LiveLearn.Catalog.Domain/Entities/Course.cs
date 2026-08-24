36c512b9a1e2f88b2de8281ca9df3bb67642283f
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-24 11:24:24 +0300

    US-CA03 Task assignment to sections
    - raised TaskRemovedFromSectionDomainEvent from Course

--- src/services/Catalog/LiveLearn.Catalog.Domain/Entities/Course.cs
+++ src/services/Catalog/LiveLearn.Catalog.Domain/Entities/Course.cs
@@ -236,6 +236,10 @@ public sealed class Course : AggregateRoot<Guid>
         var section = _sections.FirstOrDefault(s => s.Id == sectionId);
         if (section is null) return Result.Failure(CourseErrors.SectionNotFound);
         var result = section.RemoveTask(taskId);
+        
+        if (result.IsSuccess) 
+            RaiseDomainEvent(new TaskRemovedFromSectionDomainEvent(taskId, section.Id, CourseId: Id));
+
         return result;
     }
 
