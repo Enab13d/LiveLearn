8c2ef1641b0ab063835de7c9c3eac76f659ff7f1
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-24 11:25:09 +0300

    US-CA03 Task assignment to sections
    - implemented TaskRemovedFromSectionDomainEventHandler

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/DomainEventHandlers/TaskRemovedFromSectionDomainEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Domain.DomainEvents;
+using LiveLearn.Contracts.Catalog;
+
+namespace LiveLearn.Catalog.Application.DomainEventHandlers;
+
+
+public sealed class TaskRemovedFromSectionDomainEventHandler(IEventBus eventBus, TimeProvider timeProvider) : IDomainEventHandler<TaskRemovedFromSectionDomainEvent>
+{
+    public async Task Handle(TaskRemovedFromSectionDomainEvent notification, CancellationToken ct)
+    {
+        await eventBus.PublishAsync<TaskRemovedFromSectionEvent>(
+            new(notification.TaskId, notification.SectionId, notification.CourseId, timeProvider.GetUtcNow()), ct);
+    }
+}
