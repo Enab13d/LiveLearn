1872db49317cda16df6b70870cff897337181e76
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-24 10:39:02 +0300

    US-CA03 Task assignment to sections
    - implemented AssignTaskToSectionCommand

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Commands/AssignTaskToSectionCommand.cs
@@ -0,0 +1,8 @@
+﻿
+
+using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Catalog.Application.Commands;
+
+
+public sealed record AssignTaskToSectionCommand(Guid TutorId, Guid CourseId, Guid SectionId, Guid TaskId) : ICommand;
