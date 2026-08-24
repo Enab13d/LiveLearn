683f163c916cf27208b2075408268c192b13cccf
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-24 10:41:36 +0300

    US-CA03 Task assignment to sections
    - implemented handler for AssignTaskToSectionDomainEvent

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/DomainEventHandlers/AssignTaskToSectionDomainEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Domain.DomainEvents;
+using LiveLearn.Contracts.Catalog;
+
+namespace LiveLearn.Catalog.Application.DomainEventHandlers;
+
+
+internal sealed class AssignTaskToSectionDomainEventHandler(IEventBus eventBus, TimeProvider timeProvider) 
+    : IDomainEventHandler<TaskAssignedToSectionDomainEvent>
+{
+    public async Task Handle(TaskAssignedToSectionDomainEvent notification, CancellationToken ct)
+    {
+        await eventBus.PublishAsync<TaskAssignedToSectionEvent>(
+            new(notification.TaskId, notification.SectionId, notification.CourseId, timeProvider.GetUtcNow()), ct);
+    }
+}
