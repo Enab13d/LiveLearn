0b46f157bb3e3dd4802c2db59a7b1f98d2eed1ba
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-24 11:23:28 +0300

    US-CA03 Task assignment to sections
    - added TaskRemovedFromSectionDomainEvent

---
+++ src/services/Catalog/LiveLearn.Catalog.Domain/DomainEvents/TaskRemovedFromSectionDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Catalog.Domain.DomainEvents;
+
+
+public sealed record TaskRemovedFromSectionDomainEvent(
+    Guid TaskId,
+    Guid SectionId,
+    Guid CourseId
+) : IDomainEvent;
