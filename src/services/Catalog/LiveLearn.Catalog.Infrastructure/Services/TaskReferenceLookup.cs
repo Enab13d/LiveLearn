3ecb1dd070bdcf19e3007cadd937cf4a97bf504c
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-24 12:09:47 +0300

    US-CA03 Task assignment to sections
    - implemented TaskReferenceLookup

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Services/TaskReferenceLookup.cs
@@ -0,0 +1,18 @@
+﻿using LiveLearn.Catalog.Application.Dto;
+using LiveLearn.Catalog.Application.Services;
+using LiveLearn.Catalog.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Catalog.Infrastructure.Services;
+
+
+internal sealed class TaskReferenceLookup(WriteDbContext dbContext) : ITaskReferenceLookup
+{
+    public async Task<TaskReferenceDto?> GetByTaskIdAsync(Guid taskId, CancellationToken ct = default)
+    {
+        var taskReference = await dbContext.TaskReplicas.FirstOrDefaultAsync(e => e.TaskId == taskId, ct);
+        
+        return taskReference is null ? null : new TaskReferenceDto(taskReference.TaskId, taskReference.TutorId, taskReference.TaskType);
+
+    }
+}
