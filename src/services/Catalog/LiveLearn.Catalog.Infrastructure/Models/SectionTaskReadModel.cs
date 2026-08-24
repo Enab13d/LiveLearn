248dd6366d8a34ae5c54823a7fb7a3e3767befd5
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-24 11:54:18 +0300

    US-CA03 Task assignment to sections
    - scaffolded read model for SectionTask

---
+++ src/services/Catalog/LiveLearn.Catalog.Infrastructure/Models/SectionTaskReadModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveLearn.Catalog.Infrastructure.Models;
+
+internal class SectionTaskReadModel
+{
+    public Guid TaskId { get; set; }
+    public Guid SectionId { get; set; }
+    public Guid CourseId { get; set; }
+    public string TaskType { get; set; } = string.Empty;
+
+}
