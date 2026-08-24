bb662ea69723660ac913b7d0f90acc552ab517bd
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-24 11:22:49 +0300

    US-CA03 Task assignment to sections
    - added RemoveTaskFromSectionCommand

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/Commands/RemoveTaskFromSectionCommand.cs
@@ -0,0 +1,8 @@
+﻿
+
+using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Catalog.Application.Commands;
+
+
+public sealed record RemoveTaskFromSectionCommand(Guid TutorId, Guid CourseId, Guid SectionId, Guid TaskId) : ICommand;
