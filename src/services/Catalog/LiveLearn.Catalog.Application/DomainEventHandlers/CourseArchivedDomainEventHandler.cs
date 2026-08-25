ba1dde531cfa2113b2cf5863b84ee52de9be4f7b
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-25 15:08:04 +0300

    US-CA03 Task assignment to sections
    - implemented CourseArchivedDomainEventHandler

---
+++ src/services/Catalog/LiveLearn.Catalog.Application/DomainEventHandlers/CourseArchivedDomainEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.Domain.DomainEvents;
+using LiveLearn.Contracts.Catalog;
+
+namespace LiveLearn.Catalog.Application.DomainEventHandlers;
+
+internal sealed class CourseArchivedDomainEventHandler(IEventBus eventBus) : IDomainEventHandler<CourseArchivedDomainEvent>
+{
+    public async Task Handle(CourseArchivedDomainEvent notification, CancellationToken ct)
+    {
+        await eventBus.PublishAsync<CourseArchivedEvent>(new(notification.CourseId), ct);
+    }
+}
