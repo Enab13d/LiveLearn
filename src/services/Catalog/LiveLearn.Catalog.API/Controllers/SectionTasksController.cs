031582bcf52fd79eed424806eddc9a513bda3fde
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-25 16:14:29 +0300

    US-CA03 Task assignment to sections
    - implemented SectionTasksController

---
+++ src/services/Catalog/LiveLearn.Catalog.API/Controllers/SectionTasksController.cs
@@ -0,0 +1,39 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Catalog.API.Dto.Requests;
+using LiveLearn.Catalog.API.Extensions;
+using LiveLearn.Catalog.Application.Authorization;
+using LiveLearn.Catalog.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveLearn.Catalog.API.Controllers;
+
+[ApiController]
+[Authorize(Policy = AuthorizationPolicies.TutorPolicy)]
+[Route("api/courses/{courseId:guid}/sections/{sectionId:guid}/[controller]")]
+public sealed class SectionTasksController(ISender mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> AssignTaskToSection([FromBody] RequestSectionTaskDto request, Guid courseId, Guid sectionId, CancellationToken ct)
+    {
+        if (User.GetUserId() is not Guid tutorId)
+            return Problem("Token sub claim is not a valid identifier. Identity provider misconfigured.");
+
+        var result = await mediator.Send(new AssignTaskToSectionCommand(tutorId, courseId, sectionId, request.TaskId), ct);
+
+        return result.IsSuccess ? Ok() : this.ToProblemResult(result.Errors);
+    }
+
+    [HttpDelete]
+    [Route("{taskId:guid}")]
+    public async Task<IActionResult> RemoveTaskFromSection(Guid courseId, Guid sectionId, Guid taskId, CancellationToken ct)
+    {
+        if (User.GetUserId() is not Guid tutorId)
+            return Problem("Token sub claim is not a valid identifier. Identity provider misconfigured.");
+
+        var result = await mediator.Send(new RemoveTaskFromSectionCommand(tutorId, courseId, sectionId, taskId), ct);
+
+        return result.IsSuccess ? NoContent() : this.ToProblemResult(result.Errors);
+    }
+}
---
+++ src/services/Catalog/LiveLearn.Catalog.API/Dto/Requests/RequestSectionTaskAssignDto.cs
@@ -0,0 +1,5 @@
+﻿namespace LiveLearn.Catalog.API.Dto.Requests;
+
+public sealed record RequestSectionTaskDto(Guid TaskId);
+
+
