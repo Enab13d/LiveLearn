7dfc9d6ee41782c2d574083ccd6f9fbbb792fe5e
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-26 18:15:43 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded QuizErrors

---
+++ src/services/Assessment/LiveLearn.Assessment.Domain/Errors/QuizErrors.cs
@@ -0,0 +1,13 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Domain.Errors;
+
+
+public static class QuizErrors
+{
+    public static Error InvalidAnswerIndex => Error.Conflict("QuizErrors.InvalidAnswerIndex", "Index should be within answers range");
+
+    public static Error EmptyQuizEvaluation => Error.Conflict("QuizErrors.EmptyQuizEvaluation", "Cannot evaluate quiz with no questions");
+
+    public static Error QuestionNotFound => Error.NotFound("QuizErrors.QuestionNotFound", "Question not found");
+}
