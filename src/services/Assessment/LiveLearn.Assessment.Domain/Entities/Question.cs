0781e0dd96ca82a3dc5a2eebbedcb836e51345f8
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-26 15:01:01 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded Question entity

---
+++ src/services/Assessment/LiveLearn.Assessment.Domain/Entities/Question.cs
@@ -0,0 +1,27 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Domain.Entities;
+
+public sealed class Question : Entity<Guid>
+{
+    private Question() { }
+
+    internal Question(string text, List<string> answerContents, int correctAnswerIdx)
+    {
+        Id = Guid.NewGuid();
+        Text = text;
+        var createdAnswers = answerContents.Select(content => new Answer(Guid.NewGuid(), content)).ToList();
+        CorrectAnswerId = createdAnswers[correctAnswerIdx].Id;
+        foreach (var answer in createdAnswers)
+        {
+            _answers.Add(answer);
+        }
+    }
+
+    public string Text { get; private set; } = string.Empty;
+
+    private readonly List<Answer> _answers = [];
+    public IReadOnlyCollection<Answer> Answers => _answers.AsReadOnly();
+    public Guid CorrectAnswerId { get; private set; }
+
+}
