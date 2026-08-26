566b82947fc3564ddee16aec474ee1d4e11c21f8
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-26 13:54:13 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded base class for AssessmentTask

---
+++ src/services/Assessment/LiveLearn.Assessment.Domain/Entities/AssessmentTask.cs
@@ -0,0 +1,14 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Domain.Entities;
+
+
+public abstract class AssessmentTask : AggregateRoot<Guid>
+{
+
+    protected AssessmentTask() { }
+    public string Title { get; private set; } = string.Empty;
+    public Guid TutorId { get; private set; }
+    public Guid SectionId { get; private set; }
+    public Guid CourseId { get; private set; }
+}
