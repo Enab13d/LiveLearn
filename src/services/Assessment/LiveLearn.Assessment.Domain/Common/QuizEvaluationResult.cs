5a0d3902c054b6cd2237fd0ac7d26c8c2884e5dd
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-26 18:14:52 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added QuizEvaluationResult (bridge between Quiz and QuizAttempt aggregates)

---
+++ src/services/Assessment/LiveLearn.Assessment.Domain/Common/QuizEvaluationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace LiveLearn.Assessment.Domain.Common;
+
+
+public sealed record QuizEvaluationResult(
+    bool IsPassed,
+    int Score,
+    List<Guid> CorrectAnswerIds,
+    List<Guid> WrongAnswerIds
+);
