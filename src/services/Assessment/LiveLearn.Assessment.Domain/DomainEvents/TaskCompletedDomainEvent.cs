ba465c4a582702a4b12438b5c06d97cef760bd51
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-26 18:09:10 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added domain events for task completion and failure

---
+++ src/services/Assessment/LiveLearn.Assessment.Domain/DomainEvents/TaskCompletedDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Domain.DomainEvents;
+
+
+public sealed record TaskCompletedDomainEvent(
+    Guid TaskId, 
+    Guid StudentId, 
+    Guid SectionId,
+    Guid CourseId
+) : IDomainEvent;
