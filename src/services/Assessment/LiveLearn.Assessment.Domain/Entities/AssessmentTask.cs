6155cbdd5dee4511cce89a4c65ef6ba9538f1cbc
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-26 18:03:17 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - update access modifiers on AssessmentTask properties

--- src/services/Assessment/LiveLearn.Assessment.Domain/Entities/AssessmentTask.cs
+++ src/services/Assessment/LiveLearn.Assessment.Domain/Entities/AssessmentTask.cs
@@ -7,8 +7,8 @@ public abstract class AssessmentTask : AggregateRoot<Guid>
 {
 
     protected AssessmentTask() { }
-    public string Title { get; private set; } = string.Empty;
-    public Guid TutorId { get; private set; }
-    public Guid SectionId { get; private set; }
-    public Guid CourseId { get; private set; }
+    public string Title { get; protected set; } = string.Empty;
+    public Guid TutorId { get; protected set; }
+    public Guid SectionId { get; protected set; }
+    public Guid CourseId { get; protected set; }
 }
