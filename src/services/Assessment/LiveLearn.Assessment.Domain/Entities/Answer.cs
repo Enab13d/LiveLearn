646f3cdfc78e7a6c3e0c5df15ad0a85efd457e14
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-26 15:00:42 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded answer entity

---
+++ src/services/Assessment/LiveLearn.Assessment.Domain/Entities/Answer.cs
@@ -0,0 +1,15 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Domain.Entities;
+
+public sealed class Answer : Entity<Guid>
+{
+    private Answer() { }
+    public string Content { get; private set; } = string.Empty;
+
+    internal Answer(Guid id, string content)
+    {
+        Id = id;
+        Content = content;
+    }
+}
