2d035682b0c5e80fa2c5fd3338f17b1cb2db11d7
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-08-26 18:08:19 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded QuizAttempt entity

---
+++ src/services/Assessment/LiveLearn.Assessment.Domain/Entities/QuizAttempt.cs
@@ -0,0 +1,39 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Domain.Entities;
+
+
+public sealed class QuizAttempt : AggregateRoot<Guid>
+{
+    private QuizAttempt() { }
+
+    public Guid QuizId { get; private init; }
+
+    public Guid StudentId { get; private init; }
+
+    public Guid SectionId { get; private init; }
+
+    public Guid CourseId { get; private init; }
+
+    public int Score { get; private init; }
+
+    public bool IsPassed { get; private init; }
+
+    public DateTimeOffset AttemptedAt { get; private init; }
+
+    public static QuizAttempt Create(Guid id, Guid quizId, Guid studentId, Guid sectionId, Guid courseId, int score, bool isPassed)
+    {
+        QuizAttempt quizAttempt = new()
+        {
+            Id = id,
+            QuizId = quizId,
+            StudentId = studentId,
+            SectionId = sectionId,
+            CourseId = courseId,
+            Score = score,
+            IsPassed = isPassed
+        };
+
+        return quizAttempt;
+    }
+}
