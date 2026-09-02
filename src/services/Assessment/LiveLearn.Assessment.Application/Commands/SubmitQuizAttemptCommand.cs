2f787c04a13fb575637b03210d9c96f4bc977fb4
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-02 14:30:59 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded SubmitQuizAttemptCommand

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/SubmitQuizAttemptCommand.cs
@@ -0,0 +1,7 @@
+﻿using LiveLearn.Assessment.Domain.Common;
+using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Application.Commands;
+
+
+public sealed record SubmitQuizAttemptCommand(Guid StudentId, Guid QuizId, Dictionary<Guid, Guid> Answers) : ICommand<QuizEvaluationResult>;
