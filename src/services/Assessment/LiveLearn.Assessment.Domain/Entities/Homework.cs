03ec1665e807a763d21e57ed85c92b51afc0bd8f
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-02 12:38:35 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded Homework aggregate

---
+++ src/services/Assessment/LiveLearn.Assessment.Domain/Entities/Homework.cs
@@ -0,0 +1,28 @@
+﻿using LiveLearn.Assessment.Domain.DomainEvents;
+
+namespace LiveLearn.Assessment.Domain.Entities;
+
+
+public sealed class Homework : AssessmentTask
+{
+
+    private Homework() { }
+    public string Description { get; private set; } = string.Empty;
+
+    public static Homework Create(Guid id, string title, Guid tutorId, string description)
+    {
+        Homework homework = new()
+        {
+            Id = id,
+            Title = title,
+            TutorId = tutorId,
+            Description = description
+        };
+
+        homework.RaiseDomainEvent(new TaskCreatedDomainEvent(id, tutorId, nameof(Homework)));
+        return homework;
+
+    }
+
+
+}
