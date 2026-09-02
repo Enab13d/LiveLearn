6c9f5aa7153b96c8e6c17e0065c3bfc1c305b74c
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-02 14:51:09 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded AssessmentTaskRepository abstraction

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Repositories/IAssessmentTaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using LiveLearn.Assessment.Domain.Entities;
+using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Application.Repositories;
+
+
+public interface IAssessmentTaskRepository : IRepository<AssessmentTask, Guid>
+{
+    Task<Homework?> GetHomeworkAsync(Guid id, CancellationToken ct = default);
+
+    Task<Quiz?> GetQuizAsync(Guid id, CancellationToken ct = default);
+}
