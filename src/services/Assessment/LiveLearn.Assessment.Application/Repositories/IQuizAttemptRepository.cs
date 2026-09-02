fdefbeffbd1498c37ba43042292fe7c4c0eb9376
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-02 15:01:52 +0300

    US-AS01 Tutors create and manage quizzes and homework
    scaffolded IQuizAttemptRepository

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Repositories/IQuizAttemptRepository.cs
@@ -0,0 +1,10 @@
+﻿using LiveLearn.Assessment.Domain.Entities;
+using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Application.Repositories;
+
+
+public interface IQuizAttemptRepository : IRepository<QuizAttempt, Guid>
+{
+
+}
