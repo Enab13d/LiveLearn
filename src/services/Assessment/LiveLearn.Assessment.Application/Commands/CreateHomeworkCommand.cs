e81246d37f512f47f81a7f201c9732d6dba05919
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-02 14:24:40 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded CreateHomeworkCommand

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/CreateHomeworkCommand.cs
@@ -0,0 +1,6 @@
+﻿using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Application.Commands;
+
+
+public sealed record CreateHomeworkCommand(Guid TutorId, string Title, string Description) : ICommand<Guid>;
