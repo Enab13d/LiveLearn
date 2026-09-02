8573e00e5a13a59b88abe327c9ad1650c0020ebf
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-02 17:25:14 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded GetTasksByTutorQuery

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Dto/TaskSummaryDto.cs
@@ -0,0 +1,4 @@
+﻿namespace LiveLearn.Assessment.Application.Dto;
+
+
+public sealed record TaskSummaryDto(Guid Id, string Title, string TaskType, Guid SectionId, Guid CourseId);
---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Queries/GetTasksByTutorQuery.cs
@@ -0,0 +1,11 @@
+﻿using LiveLearn.Assessment.Application.Dto;
+using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Application.Queries;
+
+
+public sealed record GetTasksByTutorQuery(
+    Guid TutorId,
+    int PageNumber,
+    int PageSize
+) : IQuery<PagedResult<TaskSummaryDto>>;
