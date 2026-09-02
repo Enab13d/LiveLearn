425b69ff58722d6685b2709a5b81c9734734234e
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-02 15:02:13 +0300

    US-AS01 Tutors create and manage quizzes and homework
    scaffolded IHomeworkSubmissionRepository

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Repositories/IHomeworkSubmissionRepository.cs
@@ -0,0 +1,9 @@
+﻿using LiveLearn.Assessment.Domain.Entities;
+using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Application.Repositories;
+
+public interface IHomeworkSubmissionRepository : IRepository<HomeworkSubmission, Guid>
+{
+    
+}
