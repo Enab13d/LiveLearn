3a41eb20ab97a2bff16a308d54e34e53f15814a8
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 17:12:40 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added DeleteTaskCommandValidator

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/Validators/DeleteTaskCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace LiveLearn.Assessment.Application.Commands.Validators;
+
+
+public sealed class DeleteTaskCommandValidator : AbstractValidator<DeleteTaskCommand>
+{
+    public DeleteTaskCommandValidator()
+    {
+        RuleFor(x => x.TaskId).NotEmpty();
+        RuleFor(x => x.TutorId).NotEmpty();
+    }
+}
