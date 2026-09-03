3e5b3a1741e5c7363a2f6f1716fe65472ffddfb1
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 15:47:02 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added validator for AddQuestionToQuizCommand

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/Validators/AddQuestionToQuizCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace LiveLearn.Assessment.Application.Commands.Validators;
+
+public sealed class AddQuestionToQuizCommandValidator : AbstractValidator<AddQuestionToQuizCommand>
+{
+    public AddQuestionToQuizCommandValidator()
+    {
+        RuleFor(x => x.TutorId).NotEmpty();
+        RuleFor(x => x.QuizId).NotEmpty();
+        RuleFor(x => x.Text).NotEmpty().MinimumLength(8).MaximumLength(256);
+        RuleFor(x => x.Answers).NotEmpty().Must(l => l.Count >= 2 && l.Count <= 6);
+        RuleFor(x => x.CorrectAnswerIdx)
+            .Must((model, currentValue) => 
+                currentValue < model.Answers.Count && currentValue >= 0);
+    }
+}
