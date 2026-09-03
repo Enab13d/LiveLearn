efd580befe4eca89b419f374295c663f5a446324
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 17:27:28 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added SubmitHomeworkCommandValidator

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/Validators/SubmitHomeworkCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace LiveLearn.Assessment.Application.Commands.Validators;
+
+
+public sealed class SubmitHomeworkCommandValidator : AbstractValidator<SubmitHomeworkCommand>
+{
+    public SubmitHomeworkCommandValidator()
+    {
+        RuleFor(x => x.HomeworkId).NotEmpty();
+        RuleFor(x => x.StudentId).NotEmpty();
+        RuleFor(x => x.Content).NotEmpty()
+            .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out var parsed))
+            .WithMessage(x => $"Property {nameof(x.Content)} must be a valid URI");
+    }
+}
