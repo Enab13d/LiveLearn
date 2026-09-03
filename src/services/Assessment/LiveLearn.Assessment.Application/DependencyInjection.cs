73986f728291378ee9a4c07224bb3ef15faf2cd5
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 17:56:00 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added FlentValidation nuget packages in Assessment Application
    - added DI extenstion for Assessment Application

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using LiveLearn.BuildingBlocks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LiveLearn.Assessment.Application;
+
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddAssessmentApplication(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        return services;
+    }
+}
