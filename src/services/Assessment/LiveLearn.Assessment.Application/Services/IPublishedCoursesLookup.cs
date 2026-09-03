fc535ac2d50210bb3895e6648f517e5c9421a2db
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 12:43:25 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded IPublishedCoursesLookup

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Services/IPublishedCoursesLookup.cs
@@ -0,0 +1,6 @@
+﻿namespace LiveLearn.Assessment.Application.Services;
+
+public interface IPublishedCoursesLookup
+{
+    Task<bool> IsPublishedAsync(Guid courseId, CancellationToken ct = default);
+}
