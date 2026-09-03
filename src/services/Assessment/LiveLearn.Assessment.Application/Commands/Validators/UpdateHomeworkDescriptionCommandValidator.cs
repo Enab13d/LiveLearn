14253ec3883762dc9d492bba00d44a43be667fe0
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 17:45:06 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - UpdateHomeworkDescriptionCommandValidator

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/Validators/UpdateHomeworkDescriptionCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace LiveLearn.Assessment.Application.Commands.Validators;
+
+
+public sealed class UpdateHomeworkDescriptionCommandValidator : AbstractValidator<UpdateHomeworkDescriptionCommand>
+{
+    public UpdateHomeworkDescriptionCommandValidator()
+    {
+        RuleFor(x => x.HomeworkId).NotEmpty();
+        RuleFor(x => x.TutorId).NotEmpty();
+        RuleFor(x => x.Description).NotEmpty().MinimumLength(64).MaximumLength(4096);
+    }
+}
