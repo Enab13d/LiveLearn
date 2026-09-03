153db7e0a3d2ae278de55ed35b41ebc148258276
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 17:12:15 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added CreateQuizCommandValidator

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/Validators/CreateQuizCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace LiveLearn.Assessment.Application.Commands.Validators;
+
+
+internal sealed class CreateQuizCommandValidator : AbstractValidator<CreateQuizCommand>
+{
+    public CreateQuizCommandValidator()
+    {
+        RuleFor(x => x.TutorId).NotEmpty();
+        RuleFor(x => x.Title).NotEmpty().MinimumLength(8).MaximumLength(128);
+        RuleFor(x => x.PassingScore).InclusiveBetween(0, 100);
+    }
+
+}
