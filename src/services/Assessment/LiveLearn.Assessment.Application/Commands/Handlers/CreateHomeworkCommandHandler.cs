1987d32b88da4c88b7dedf46ce4646f74fdfdba4
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 13:53:15 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - implemented CreateHomeworkCommandHandler

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/Handlers/CreateHomeworkCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using LiveLearn.Assessment.Application.Repositories;
+using LiveLearn.Assessment.Domain.Entities;
+using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Application.Commands.Handlers;
+
+
+internal sealed class CreateHomeworkCommandHandler(IAssessmentTaskRepository repository, IUnitOfWork unitOfWork) : ICommandHandler<CreateHomeworkCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(CreateHomeworkCommand request, CancellationToken ct)
+    {
+        var (tutorId, title, description) = request;
+        var homeworkId = Guid.NewGuid();
+        var homework = Homework.Create(homeworkId, title, tutorId, description);
+        await repository.AddAsync(homework, ct);
+        await unitOfWork.CommitAsync(ct);
+        return homeworkId;
+    }
+}
