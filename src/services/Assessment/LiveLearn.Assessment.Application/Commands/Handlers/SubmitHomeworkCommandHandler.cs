517db4a755edeb3fe88e004d33cbe90cbcb39e06
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 15:10:19 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - implemented SubmitHomeworkCommandHandler

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/Handlers/SubmitHomeworkCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using LiveLearn.Assessment.Application.Repositories;
+using LiveLearn.Assessment.Domain.Entities;
+using LiveLearn.Assessment.Domain.Enums;
+using LiveLearn.Assessment.Domain.Errors;
+using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Application.Commands.Handlers;
+
+
+internal sealed class SubmitHomeworkCommandHandler(
+    IAssessmentTaskRepository assessmentTaskRepository,
+    IHomeworkSubmissionRepository homeworkSubmissionRepository,
+    IUnitOfWork unitOfWork
+) : ICommandHandler<SubmitHomeworkCommand>
+{
+    public async Task<Result> Handle(SubmitHomeworkCommand request, CancellationToken ct)
+    {
+
+        var homework = await assessmentTaskRepository.GetHomeworkAsync(request.HomeworkId, ct);
+        if (homework is null) return Result.Failure(TaskErrors.NotFound);
+
+        var existingSubmissions = await homeworkSubmissionRepository
+            .FindAsync(
+                e => e.HomeworkId == request.HomeworkId
+                && e.Status == HomeworkStatus.PendingReview
+                && e.StudentId == request.StudentId, 
+                ct);
+
+        if (existingSubmissions.Any()) return Result.Failure(HomeworkErrors.SubmissionAlreadyPending);
+
+        var submission = HomeworkSubmission.Create(
+            Guid.NewGuid(), request.HomeworkId, request.StudentId, homework.SectionId, homework.CourseId, request.Content);
+
+        await homeworkSubmissionRepository.AddAsync(submission, ct);
+        await unitOfWork.CommitAsync(ct);
+
+        return Result.Success();
+    }
+}
