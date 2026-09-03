b349f61be6d6f9037f8eef1022c18b5f87ee3368
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 18:12:58 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded WriteDbContext

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/Contexts/WriteDbContext.cs
@@ -0,0 +1,31 @@
+﻿using LiveLearn.Assessment.Domain.Entities;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Assessment.Infrastructure.Contexts;
+
+
+public sealed class WriteDbContext(DbContextOptions<WriteDbContext> options) : DbContext(options)
+{
+    public DbSet<AssessmentTask> AssessmentTasks { get; set; } = null!;
+
+    public DbSet<Quiz> Quizzes { get; set; } = null!;
+
+    public DbSet<Homework> Homeworks { get; set; } = null!;
+
+    public DbSet<HomeworkSubmission> HomeworkSubmissions { get; set; } = null!;
+
+    public DbSet<QuizAttempt> QuizAttempts { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<AssessmentTask>().UseTptMappingStrategy();
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(WriteDbContext).Assembly,
+            a => a.AssemblyQualifiedName?.Contains("EntityConfigurations.Write") == true);
+
+        modelBuilder.AddTransactionalOutboxEntities();
+    }
+}
