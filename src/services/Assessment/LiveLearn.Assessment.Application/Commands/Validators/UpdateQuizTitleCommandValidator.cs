9e23e269652d68b4fc5f84f114207cd3cc33f43e
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 17:50:19 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added validator for UpdateQuizTitleCommand

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/Validators/UpdateQuizTitleCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace LiveLearn.Assessment.Application.Commands.Validators;
+
+
+public sealed class UpdateQuizTitleCommandValidator : AbstractValidator<UpdateQuizTitleCommand>
+{
+
+    public UpdateQuizTitleCommandValidator()
+    {
+        RuleFor(x => x.QuizId).NotEmpty();
+        RuleFor(x => x.TutorId).NotEmpty();
+        RuleFor(x => x.NewTitle).NotEmpty().MinimumLength(8).MaximumLength(128);
+    }
+}
