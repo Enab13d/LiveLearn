0da968202c40a53a4407320aed09c221d18f565d
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 11:51:34 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded GetQuizAttemptsQuery

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Dto/QuizAttemptDto.cs
@@ -0,0 +1,4 @@
+﻿namespace LiveLearn.Assessment.Application.Dto;
+
+
+public sealed record QuizAttemptDto(Guid Id, Guid SectionId, Guid CourseId, int Score, bool IsPassed, DateTimeOffset AttemptedAt);
---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Queries/GetQuizAttemptsQuery.cs
@@ -0,0 +1,7 @@
+﻿using LiveLearn.Assessment.Application.Dto;
+using LiveLearn.BuildingBlocks;
+
+namespace LiveLearn.Assessment.Application.Queries;
+
+
+public sealed record GetQuizAttemptsQuery(Guid StudentId, Guid QuizId, int PageNumber = 1, int PageSize = 10) : IQuery<PagedResult<QuizAttemptDto>>;
