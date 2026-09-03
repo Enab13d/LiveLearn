0c404fbbd33f66c9a87760d6cad3585c247b1c06
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-03 17:39:32 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added SubmitQuizAttemptCommandValidator

---
+++ src/services/Assessment/LiveLearn.Assessment.Application/Commands/Validators/SubmitQuizAttemptCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace LiveLearn.Assessment.Application.Commands.Validators;
+
+
+public sealed class SubmitQuizAttemptCommandValidator : AbstractValidator<SubmitQuizAttemptCommand>
+{
+    public SubmitQuizAttemptCommandValidator()
+    {
+        RuleFor(x => x.QuizId).NotEmpty();
+        RuleFor(x => x.StudentId).NotEmpty();
+        RuleFor(x => x.Answers).NotEmpty();
+        RuleForEach(x => x.Answers).Must(kvp => kvp.Key != Guid.Empty && kvp.Value != Guid.Empty);
+    }
+}
