2e0d3e5496b5d22f689c3c0f73bffce815b3eae2
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-04 14:51:15 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - scaffolded QuizReadModel

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/Models/QuizReadModel.cs
@@ -0,0 +1,16 @@
+﻿namespace LiveLearn.Assessment.Infrastructure.Models;
+
+
+internal sealed class QuizReadModel
+{
+    public Guid Id { get; set; }
+
+    public Guid TutorId { get; set; }
+    public Guid SectionId { get; set; }
+    public Guid CourseId { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+    public int PassingScore { get; set; }
+
+    public List<QuestionReadModel> Questions { get; set; } = [];
+}
