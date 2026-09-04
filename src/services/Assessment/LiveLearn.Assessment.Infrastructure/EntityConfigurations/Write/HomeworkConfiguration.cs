4a2174e97affd111869e453eab4e94770205ef8d
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-04 13:34:28 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added write configuration for Homework

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/EntityConfigurations/Write/HomeworkConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using LiveLearn.Assessment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LiveLearn.Assessment.Infrastructure.EntityConfigurations.Write;
+
+
+internal sealed class HomeworkConfiguration : IEntityTypeConfiguration<Homework>
+{
+    public void Configure(EntityTypeBuilder<Homework> builder)
+    {
+        builder.ToTable("homeworks");
+
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Description).IsRequired().HasMaxLength(4096);
+    }
+}
