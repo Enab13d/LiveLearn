6269af2029e27088947e33630db0c44d087c4738
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-04 13:50:45 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added write configuration for HomeworkSubmission

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/EntityConfigurations/Write/HomeworkSubmissionConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using LiveLearn.Assessment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LiveLearn.Assessment.Infrastructure.EntityConfigurations.Write;
+
+
+internal sealed class HomeworkSubmissionConfiguration : IEntityTypeConfiguration<HomeworkSubmission>
+{
+    public void Configure(EntityTypeBuilder<HomeworkSubmission> builder)
+    {
+        builder.ToTable("homework_submissions");
+
+        builder.HasKey(e => e.Id);
+        builder.HasOne<Homework>()
+            .WithMany()
+            .HasForeignKey(e => e.HomeworkId);
+
+        builder.Property(e => e.CourseId).IsRequired();
+        builder.Property(e => e.SectionId).IsRequired();
+        builder.Property(e => e.StudentId).IsRequired();
+        builder.Property(e => e.Content).IsRequired();
+        builder.Property(e => e.Status).IsRequired().HasConversion<string>();
+        builder.Property(e => e.InstructorFeedback).HasMaxLength(2048);
+        builder.Property(e => e.SubmittedAt).ValueGeneratedOnAdd().HasDefaultValueSql("now()");
+    }
+}
