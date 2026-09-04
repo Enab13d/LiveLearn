1d179ebef975abc3e9024d8bc13d772f77057e4c
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-04 14:01:04 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added write configuration for QuizAttempt

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/EntityConfigurations/Write/QuizAttemptConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using LiveLearn.Assessment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LiveLearn.Assessment.Infrastructure.EntityConfigurations.Write;
+
+
+internal sealed class QuizAttemptConfiguration : IEntityTypeConfiguration<QuizAttempt>
+{
+    public void Configure(EntityTypeBuilder<QuizAttempt> builder)
+    {
+        builder.ToTable("quiz_attempts");
+
+        builder.HasKey(e => e.Id);
+        builder.HasOne<Quiz>().WithMany().HasForeignKey(e => e.QuizId);
+        builder.Property(e => e.Score).IsRequired();
+        builder.Property(e => e.AttemptedAt).ValueGeneratedOnAdd().HasDefaultValueSql("now()");
+        builder.HasIndex(e => new { e.QuizId, e.StudentId, e.AttemptedAt })
+            .IsDescending()
+            .HasDatabaseName("IX_QuizAttempts_Quiz_Student_AttemptedAt");
+    }
+}
