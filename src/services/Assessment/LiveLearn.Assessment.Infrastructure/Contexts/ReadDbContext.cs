d5bc3644d5a04f40142834a4dde29b239f0cdaad
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-04 14:57:01 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - created Assessment ReadDbContext

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/Contexts/ReadDbContext.cs
@@ -0,0 +1,26 @@
+﻿using LiveLearn.Assessment.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Assessment.Infrastructure.Contexts;
+
+
+internal sealed class ReadDbContext(DbContextOptions<ReadDbContext> options) : DbContext(options)
+{
+    public DbSet<AssessmentTaskReadModel> AssessmentTasks { get; set; } = null!;
+
+    public DbSet<QuizReadModel> Quizzes { get; set; } = null!;
+
+    public DbSet<HomeworkReadModel> Homeworks { get; set; } = null!;
+
+    public DbSet<HomeworkSubmissionReadModel> HomeworkSubmissions { get; set; } = null!;
+
+    public DbSet<QuizAttemptReadModel> QuizAttempts { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ReadDbContext).Assembly,
+            a => a.AssemblyQualifiedName?.Contains("EntityConfigurations.Read") == true
+        );
+    }
+}
