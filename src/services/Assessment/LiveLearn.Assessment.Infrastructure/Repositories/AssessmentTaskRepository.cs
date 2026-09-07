7fee732155a159583bc9195c55776ae1da76b2f6
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-07 13:48:50 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - implemented AssessmentTaskRepository

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/Repositories/AssessmentTaskRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+using LiveLearn.Assessment.Application.Repositories;
+using LiveLearn.Assessment.Domain.Entities;
+using LiveLearn.Assessment.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LiveLearn.Assessment.Infrastructure.Repositories;
+
+internal sealed class AssessmentTaskRepository(WriteDbContext dbContext) : IAssessmentTaskRepository
+{
+    public async Task AddAsync(AssessmentTask entity, CancellationToken ct = default)
+    {
+        await dbContext.AssessmentTasks.AddAsync(entity, ct);
+    }
+
+    public void Delete(AssessmentTask entity)
+    {
+        dbContext.AssessmentTasks.Remove(entity);
+    }
+
+    public async Task<IEnumerable<AssessmentTask>> FindAsync(Expression<Func<AssessmentTask, bool>> predicate, CancellationToken ct = default)
+    {
+        return await dbContext.AssessmentTasks.Where(predicate).ToListAsync(ct);
+    }
+
+    public async Task<AssessmentTask?> GetByIdAsync(Guid id, CancellationToken ct = default)
+    {
+        return await dbContext.AssessmentTasks.FindAsync([id], ct);
+    }
+
+    public async Task<Homework?> GetHomeworkAsync(Guid id, CancellationToken ct = default)
+    {
+        return await dbContext.Homeworks.FirstOrDefaultAsync(ct);
+    }
+
+    public async Task<Quiz?> GetQuizAsync(Guid id, CancellationToken ct = default)
+    {
+        return await dbContext.Quizzes.FirstOrDefaultAsync(ct);
+    }
+
+    public void Update(AssessmentTask entity)
+    {
+        dbContext.AssessmentTasks.Update(entity);
+    }
+}
