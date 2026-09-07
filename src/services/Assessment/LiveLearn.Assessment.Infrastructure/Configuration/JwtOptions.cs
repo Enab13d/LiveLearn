826a939a432c65d93cee50e1b608921d56f4070b
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-07 18:14:00 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added Configuration options

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/Configuration/JwtOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace LiveLearn.Assessment.Infrastructure.Configuration;
+
+public class JwtOptions
+{
+
+    public string? Authority { get; set; }
+    public string? ValidIssuer { get; set; }
+    public string? ValidAudience { get; set; }
+    public string? MetadataAddress { get; set; }
+    public string? AuthorizationUrl { get; set; }
+    public string? TokenUrl { get; set; }
+}
+
---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/Configuration/RabbitMQOptions.cs
@@ -0,0 +1,14 @@
+﻿
+namespace LiveLearn.Assessment.Infrastructure.Configuration;
+
+internal class RabbitMQOptions
+{
+    public static string SectionName => "RabbitMQ";
+    public string Host { get; set; } = string.Empty;
+
+    public string VirtualHost { get; set; } = "/";
+    public string Username { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
+
