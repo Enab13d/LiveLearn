f3e2386828d72ebd77e2d430dff78bc375df1147
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-07 14:38:47 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - implemented UnitOfWork

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using LiveLearn.BuildingBlocks;
+using LiveLearn.Assessment.Infrastructure.Contexts;
+using MediatR;
+
+namespace LiveLearn.Assessment.Infrastructure;
+
+
+internal sealed class UnitOfWork(WriteDbContext dbContext, IPublisher publisher) : IUnitOfWork
+{
+    public async Task<int> CommitAsync(CancellationToken ct = default)
+    {
+        var domainEvents = dbContext.ChangeTracker
+                    .Entries<AggregateRoot<Guid>>()
+                    .SelectMany(e => e.Entity.DomainEvents)
+                    .ToList();
+
+        foreach (var domainEvent in domainEvents)
+            await publisher.Publish(domainEvent, ct);
+
+        dbContext.ChangeTracker
+            .Entries<AggregateRoot<Guid>>()
+            .ToList()
+            .ForEach(e => e.Entity.ClearDomainEvents());
+
+        return await dbContext.SaveChangesAsync(ct);
+    }
+}
+
