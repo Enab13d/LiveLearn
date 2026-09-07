f024fe58838c31e97e3c043ddb39beceeba77b71
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-07 17:44:46 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - implemented TaskCompletedDomainEventHandler and added respective shared contract

---
+++ src/Shared/LiveLearn.Contracts/Assessment/TaskCompletedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveLearn.Contracts.Assessment;
+
+
+public sealed record TaskCompletedEvent(
+    Guid TaskId,
+    Guid StudentId,
+    Guid SectionId,
+    Guid CourseId,
+    DateTimeOffset OccurredOn
+);
---
+++ src/services/Assessment/LiveLearn.Assessment.Application/DomainEventHandlers/TaskCompletedDomainEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using LiveLearn.Assessment.Domain.DomainEvents;
+using LiveLearn.BuildingBlocks;
+using LiveLearn.Contracts.Assessment;
+
+namespace LiveLearn.Assessment.Application.DomainEventHandlers;
+
+
+internal sealed class TaskCompletedDomainEventHandler(
+    IEventBus eventBus,
+    TimeProvider timeProvider
+) : IDomainEventHandler<TaskCompletedDomainEvent>
+{
+    public async Task Handle(TaskCompletedDomainEvent notification, CancellationToken ct)
+    {
+        var (taskId, studentId, sectionId, courseId) = notification;
+        await eventBus.PublishAsync<TaskCompletedEvent>(
+            new(taskId, studentId, sectionId, courseId, timeProvider.GetUtcNow()), ct);
+    }
+}
