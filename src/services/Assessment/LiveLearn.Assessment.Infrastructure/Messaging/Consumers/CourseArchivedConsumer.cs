5d592c756812a0b0e219725401c62bb7d8d496f3
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-07 16:30:17 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - implemented CourseArchivedConsumer

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/Messaging/Consumers/CourseArchivedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using LiveLearn.Assessment.Infrastructure.Contexts;
+using LiveLearn.Contracts.Catalog;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveLearn.Assessment.Infrastructure.Messaging.Consumers;
+
+
+internal sealed class CourseArchivedConsumer(WriteDbContext dbContext) : IConsumer<CourseArchivedEvent>
+{
+    public async Task Consume(ConsumeContext<CourseArchivedEvent> context)
+    {
+
+        await dbContext.LockedTasks
+            .Where(e => e.CourseId == context.Message.CourseId)
+            .ExecuteDeleteAsync(context.CancellationToken);    
+
+    }
+}
+
+internal sealed class CourseArchivedConsumerDefinition : ConsumerDefinition<CourseArchivedConsumer>
+{
+
+    protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator, IConsumerConfigurator<CourseArchivedConsumer> consumerConfigurator, IRegistrationContext context)
+    {
+        endpointConfigurator.UseEntityFrameworkOutbox<WriteDbContext>(context);
+    }
+}
