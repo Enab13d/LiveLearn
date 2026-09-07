e88c7bfa1b012bd2ff01094e181023568f644468
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-07 15:57:50 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - implemented TaskRemovedFromSectionConsumer

---
+++ src/services/Assessment/LiveLearn.Assessment.Infrastructure/Messaging/Consumers/TaskRemovedFromSectionConsumer.cs
@@ -0,0 +1,18 @@
+﻿using LiveLearn.Assessment.Application.Commands;
+using LiveLearn.Contracts.Catalog;
+using MassTransit;
+using MediatR;
+
+namespace LiveLearn.Assessment.Infrastructure.Messaging.Consumers;
+
+
+internal sealed class TaskRemovedFromSectionConsumer(
+    ISender mediator
+) : IConsumer<TaskRemovedFromSectionEvent>
+{
+    public async Task Consume(ConsumeContext<TaskRemovedFromSectionEvent> context)
+    {
+        await mediator.Send(new UnassignTaskCommand(context.Message.TaskId), context.CancellationToken);
+
+    }
+}
