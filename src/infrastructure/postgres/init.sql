8d032ed395d300f337f830a10a3204ed1ce872d8
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-07 19:13:30 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - added assessment db in init.sql

--- src/infrastructure/postgres/init.sql
+++ src/infrastructure/postgres/init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE keycloak;
 CREATE DATABASE livelearn_identity;
 CREATE DATABASE livelearn_catalog;
+CREATE DATABASE livelearn_assessment;
 CREATE DATABASE livelearn_enrollment;
 CREATE DATABASE livelearn_payment;
 CREATE DATABASE livelearn_certificate;
