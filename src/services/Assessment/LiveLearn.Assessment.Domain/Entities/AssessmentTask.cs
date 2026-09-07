acdae6942df0e07ca4d77413dc473eb8dcc1591a
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-07 11:53:28 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - made SectionId and CourseId in AssessmentTask nullable, reason: prevent empty guids in table, since would be populated from integration events

--- src/services/Assessment/LiveLearn.Assessment.Domain/Entities/AssessmentTask.cs
+++ src/services/Assessment/LiveLearn.Assessment.Domain/Entities/AssessmentTask.cs
@@ -10,8 +10,8 @@ public abstract class AssessmentTask : AggregateRoot<Guid>
     protected AssessmentTask() { }
     public string Title { get; protected set; } = string.Empty;
     public Guid TutorId { get; protected set; }
-    public Guid SectionId { get; protected set; }
-    public Guid CourseId { get; protected set; }
+    public Guid? SectionId { get; protected set; }
+    public Guid? CourseId { get; protected set; }
 
     public void MarkAsDeleted() => RaiseDomainEvent(new TaskDeletedDomainEvent(Id));
 }
