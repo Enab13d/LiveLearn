42bb04ea30eec651919002f2fe3bafea18127159
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2026-09-08 18:01:03 +0300

    US-AS01 Tutors create and manage quizzes and homework
    - implemented QuizzesController

---
+++ src/services/Assessment/LiveLearn.Assessment.API/Controllers/QuizzesController.cs
@@ -0,0 +1,120 @@
+﻿using LiveLearn.Assessment.API.Dto.Requests;
+using LiveLearn.Assessment.API.Extensions;
+using LiveLearn.Assessment.Application.Authorization;
+using LiveLearn.Assessment.Application.Commands;
+using LiveLearn.Assessment.Application.Queries;
+using LiveLearn.BuildingBlocks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveLearn.Assessment.API.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/[controller]")]
+public sealed class QuizzesController(ISender mediator) : ControllerBase
+{
+    [HttpPost]
+    [Authorize(Policy = AuthorizationPolicies.TutorPolicy)]
+    public async Task<IActionResult> CreateQuiz([FromBody] CreateQuizRequestDto request, CancellationToken ct)
+    {
+        if (User.GetUserId() is not Guid tutorId)
+            return Problem("Token sub claim is not a valid identifier. Identity provider misconfigured.");
+
+        var result = await mediator.Send(new CreateQuizCommand(tutorId, request.Title, request.PassingScore), ct);
+
+        return result.IsSuccess
+            ? CreatedAtAction(nameof(GetQuiz), new { taskId = result.Value }, result.Value)
+            : this.ToProblemResult(result.Errors);
+    }
+
+    [HttpDelete]
+    [Route("{taskId:guid}")]
+    [Authorize(Policy = AuthorizationPolicies.TutorPolicy)]
+    public async Task<IActionResult> DeleteQuiz(Guid taskId, CancellationToken ct)
+    {
+        if (User.GetUserId() is not Guid tutorId)
+            return Problem("Token sub claim is not a valid identifier. Identity provider misconfigured.");
+
+        var result = await mediator.Send(new DeleteTaskCommand(tutorId, taskId), ct);
+
+        return result.IsSuccess ? NoContent() : this.ToProblemResult(result.Errors);
+    }
+
+    [HttpPost]
+    [Route("{taskId:guid}/questions")]
+    [Authorize(Policy = AuthorizationPolicies.TutorPolicy)]
+    public async Task<IActionResult> AddQuestion(Guid taskId, [FromBody] AddQuestionToQuizRequest request, CancellationToken ct)
+    {
+        if (User.GetUserId() is not Guid tutorId)
+            return Problem("Token sub claim is not a valid identifier. Identity provider misconfigured.");
+
+        var result = await mediator.Send(
+            new AddQuestionToQuizCommand(tutorId, taskId, request.Text, request.Answers, request.CorrectAnswerIdx), ct);
+
+        return result.IsSuccess
+            ? Created()
+            : this.ToProblemResult(result.Errors);
+    }
+
+    [HttpDelete]
+    [Route("{taskId:guid}/questions/{questionId:guid}")]
+    [Authorize(Policy = AuthorizationPolicies.TutorPolicy)]
+    public async Task<IActionResult> RemoveQuestion(Guid taskId, Guid questionId, CancellationToken ct)
+    {
+        if (User.GetUserId() is not Guid tutorId)
+            return Problem("Token sub claim is not a valid identifier. Identity provider misconfigured.");
+
+        var result = await mediator.Send(new RemoveQuestionFromQuizCommand(taskId, tutorId, questionId), ct);
+
+        return result.IsSuccess
+            ? NoContent()
+            : this.ToProblemResult(result.Errors);
+    }
+
+    [HttpGet]
+    [Route("{taskId:guid}")]
+    public async Task<IActionResult> GetQuiz(Guid taskId, CancellationToken ct)
+    {
+        var isTutor = User.IsInRole(Role.Tutor.ToString());
+
+        var result = await mediator.Send(new GetQuizByIdQuery(taskId, IncludeCorrectAnswer: isTutor), ct);
+
+        return result.IsSuccess
+            ? Ok(result.Value)
+            : this.ToProblemResult(result.Errors);
+    }
+
+    [HttpPost]
+    [Route("{taskId:guid}/attempts")]
+    [Authorize(Policy = AuthorizationPolicies.StudentPolicy)]
+    public async Task<IActionResult> SubmitAttempt(Guid taskId, [FromBody] SubmitQuizAttemptRequestDto request, CancellationToken ct)
+    {
+        if (User.GetUserId() is not Guid studentId)
+            return Problem("Token sub claim is not a valid identifier. Identity provider misconfigured.");
+
+        var result = await mediator.Send(new SubmitQuizAttemptCommand(studentId, taskId, request.Answers), ct);
+        return result.IsSuccess
+            ? Ok(result.Value)
+            : this.ToProblemResult(result.Errors);
+
+    }
+
+    [HttpGet]
+    [Route("{taskId:guid}/attempts")]
+    [Authorize(Policy = AuthorizationPolicies.StudentPolicy)]
+    public async Task<IActionResult> GetAttempts(Guid taskId, int pageNumber, int pageSize, CancellationToken ct)
+    {
+        if (User.GetUserId() is not Guid studentId)
+            return Problem("Token sub claim is not a valid identifier. Identity provider misconfigured.");
+
+        var result = await mediator.Send(new GetQuizAttemptsQuery(studentId, taskId, pageNumber, pageSize), ct);
+
+        return result.IsSuccess
+            ? Ok(result.Value)
+            : this.ToProblemResult(result.Errors);
+    }
+
+
+}
